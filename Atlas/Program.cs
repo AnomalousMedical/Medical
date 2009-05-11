53430b4f70fa9c1ab333e44b2a12dfa2affa182f
Andrew Piper <threax@users.noreply.github.com>
2009-05-11 14:18:31 +0000

    Added Common project.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@4 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Atlas/Program.cs
+++ Atlas/Program.cs
@@ -15,7 +15,7 @@ namespace Atlas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            
         }
     }
 }
