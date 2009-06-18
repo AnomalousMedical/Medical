08f806da94d4167f89d01bed89584bdb8952ebaf
Andrew Piper <threax@users.noreply.github.com>
2009-06-18 16:10:18 +0000

    Capped framerate at 60.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@83 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Common/Controller/MedicalController.cs
+++ Common/Controller/MedicalController.cs
@@ -111,6 +111,7 @@ namespace Medical
             //Intialize the platform
             systemTimer = pluginManager.PlatformPlugin.createTimer();
             mainTimer = new UpdateTimer(systemTimer, new WindowsFormsUpdate());
+            mainTimer.FramerateCap = 60.0;
             inputHandler = pluginManager.PlatformPlugin.createInputHandler(mainForm, false, false, false);
             eventManager = new EventManager(inputHandler);
             eventUpdate = new EventUpdateListener(eventManager);
