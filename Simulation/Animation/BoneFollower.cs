63aaaffd26f0f1bb723fed9199251bda5ec743e2
Andrew Piper <threax@users.noreply.github.com>
2009-09-14 18:38:22 +0000

    Added a BoneFollower.cs class that follows a bone with a sim object. Added some Embedded artwork for an arrow.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@266 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ Simulation/Animation/BoneFollower.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine.Editing;
+using Engine.Attributes;
+using OgreWrapper;
+using Engine.ObjectManagement;
+using Engine;
+using Engine.Platform;
+using OgrePlugin;
+
+namespace Medical
+{
+    class BoneFollower : Behavior
+    {
+        [Editable]
+        String targetSimObject;
+
+        [Editable]
+        String targetNode;
+
+        [Editable]
+        String targetEntity;
+
+        [Editable]
+        String targetBone;
+
+        SkeletonInstance skeleton;
+        SimObject targetObject;
+        Bone bone;
+
+        [DoNotSave]
+        [DoNotCopy]
+        Vector3 offset;
+
+        [DoNotSave]
+        [DoNotCopy]
+        Vector3 lastPosition;
+
+        protected override void constructed()
+        {
+            targetObject = Owner.getOtherSimObject(targetSimObject);
+            if (targetObject != null)
+            {
+                SceneNodeElement node = targetObject.getElement(targetNode) as SceneNodeElement;
+                if (node != null)
+                {
+                    Entity entity = node.getNodeObject(targetEntity) as Entity;
+                    if (entity != null)
+                    {
+                        if (entity.hasSkeleton())
+                        {
+                            skeleton = entity.getSkeleton();
+                            if (skeleton.hasBone(targetBone))
+                            {
+                                bone = skeleton.getBone(targetBone);
+                                offset = Owner.Translation - targetObject.Translation - bone.getDerivedPosition();
+                                //calculate the scale at 100%
+                                Vector3 adjust = (Vector3.ScaleIdentity - Owner.Scale) * offset;
+                                offset += adjust;
+                            }
+                            else
+                            {
+                                blacklist("Entity {0} does not have a bone named {1}.", targetEntity, targetBone);
+                            }
+                        }
+                        else
+                        {
+                            blacklist("Entity {0} does not have a skeleton.", targetEntity);
+                        }
+                    }
+                    else
+                    {
+                        blacklist("Could not find Entity {0}.", targetEntity);
+                    }
+                }
+                else
+                {
+                    blacklist("Could not find target SceneNodeElement {0}.", targetNode);
+                }
+            }
+            else
+            {
+                blacklist("Could not find Target SimObject {0}.", targetSimObject);
+            }
+        }
+
+        public override void update(Clock clock, EventManager eventManager)
+        {
+            Vector3 bonePos = bone.getDerivedPosition();
+            if (bonePos != lastPosition)
+            {
+                this.updateScale(bone.getDerivedScale());
+                this.updateTranslation(targetObject.Translation + bonePos + offset * Owner.Scale);
+                lastPosition = bonePos;
+            }
+        }
+    }
+}
