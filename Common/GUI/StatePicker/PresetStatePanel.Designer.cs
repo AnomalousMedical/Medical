185903dd2acd73e0357c63b4634b3403a8630be6
Andrew Piper <threax@users.noreply.github.com>
2009-10-13 12:49:44 +0000

    Fixed bug where closing the program with an archive file caused errors.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@395 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Common/GUI/StatePicker/PresetStatePanel.Designer.cs
+++ Common/GUI/StatePicker/PresetStatePanel.Designer.cs
@@ -13,7 +13,6 @@
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            clearImages();
             if (disposing && (components != null))
             {
                 components.Dispose();
