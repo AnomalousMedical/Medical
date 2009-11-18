8250fc7e07f092171633aea27740e9b6bd6ed865
Andrew Piper <threax@users.noreply.github.com>
2009-11-18 15:00:59 +0000

    Updated the notes panel to use krypton components.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@548 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Common/GUI/StatePicker/NotesPanel.Designer.cs
+++ Common/GUI/StatePicker/NotesPanel.Designer.cs
@@ -28,98 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.datePicker = new System.Windows.Forms.DateTimePicker();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.stateNameTextBox = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.procedureType = new System.Windows.Forms.TextBox();
-            this.notes = new System.Windows.Forms.RichTextBox();
+            this.kryptonLabel4 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.stateNameTextBox = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.kryptonLabel3 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.datePicker = new ComponentFactory.Krypton.Toolkit.KryptonDateTimePicker();
+            this.kryptonLabel2 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.procedureType = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.notes = new ComponentFactory.Krypton.Toolkit.KryptonRichTextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // datePicker
-            // 
-            this.datePicker.Location = new System.Drawing.Point(6, 58);
-            this.datePicker.Name = "datePicker";
-            this.datePicker.Size = new System.Drawing.Size(200, 20);
-            this.datePicker.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 42);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Procedure Date";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 85);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Procedure Type";
-            // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.kryptonLabel4);
             this.panel1.Controls.Add(this.stateNameTextBox);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.procedureType);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.kryptonLabel3);
             this.panel1.Controls.Add(this.datePicker);
+            this.panel1.Controls.Add(this.kryptonLabel2);
+            this.panel1.Controls.Add(this.procedureType);
+            this.panel1.Controls.Add(this.kryptonLabel1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(273, 142);
+            this.panel1.Size = new System.Drawing.Size(273, 158);
             this.panel1.TabIndex = 3;
+            // 
+            // kryptonLabel4
+            // 
+            this.kryptonLabel4.Location = new System.Drawing.Point(3, 0);
+            this.kryptonLabel4.Name = "kryptonLabel4";
+            this.kryptonLabel4.Size = new System.Drawing.Size(70, 19);
+            this.kryptonLabel4.TabIndex = 13;
+            this.kryptonLabel4.Values.Text = "State Name";
             // 
             // stateNameTextBox
             // 
-            this.stateNameTextBox.Location = new System.Drawing.Point(6, 20);
+            this.stateNameTextBox.Location = new System.Drawing.Point(6, 21);
             this.stateNameTextBox.Name = "stateNameTextBox";
             this.stateNameTextBox.Size = new System.Drawing.Size(200, 20);
-            this.stateNameTextBox.TabIndex = 6;
+            this.stateNameTextBox.TabIndex = 12;
             this.stateNameTextBox.Text = "Custom State";
             // 
-            // label4
+            // kryptonLabel3
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 4);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 13);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "State Name";
+            this.kryptonLabel3.Location = new System.Drawing.Point(3, 45);
+            this.kryptonLabel3.Name = "kryptonLabel3";
+            this.kryptonLabel3.Size = new System.Drawing.Size(90, 19);
+            this.kryptonLabel3.TabIndex = 11;
+            this.kryptonLabel3.Values.Text = "Procedure Date";
             // 
-            // label3
+            // datePicker
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 127);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Notes";
+            this.datePicker.Location = new System.Drawing.Point(6, 66);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(200, 21);
+            this.datePicker.TabIndex = 10;
+            // 
+            // kryptonLabel2
+            // 
+            this.kryptonLabel2.Location = new System.Drawing.Point(3, 91);
+            this.kryptonLabel2.Name = "kryptonLabel2";
+            this.kryptonLabel2.Size = new System.Drawing.Size(91, 19);
+            this.kryptonLabel2.TabIndex = 9;
+            this.kryptonLabel2.Values.Text = "Procedure Type";
             // 
             // procedureType
             // 
-            this.procedureType.Enabled = false;
-            this.procedureType.Location = new System.Drawing.Point(6, 101);
+            this.procedureType.Location = new System.Drawing.Point(6, 112);
             this.procedureType.Name = "procedureType";
             this.procedureType.Size = new System.Drawing.Size(200, 20);
-            this.procedureType.TabIndex = 3;
+            this.procedureType.TabIndex = 8;
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Location = new System.Drawing.Point(3, 135);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(41, 19);
+            this.kryptonLabel1.TabIndex = 7;
+            this.kryptonLabel1.Values.Text = "Notes";
             // 
             // notes
             // 
             this.notes.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.notes.Location = new System.Drawing.Point(0, 142);
+            this.notes.Location = new System.Drawing.Point(0, 158);
             this.notes.Name = "notes";
-            this.notes.Size = new System.Drawing.Size(273, 179);
+            this.notes.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.notes.Size = new System.Drawing.Size(273, 163);
             this.notes.TabIndex = 4;
             this.notes.Text = "";
             // 
@@ -142,14 +138,14 @@
 
         #endregion
 
-        private System.Windows.Forms.DateTimePicker datePicker;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox procedureType;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox notes;
-        private System.Windows.Forms.TextBox stateNameTextBox;
-        private System.Windows.Forms.Label label4;
+        private ComponentFactory.Krypton.Toolkit.KryptonRichTextBox notes;
+        private ComponentFactory.Krypton.Toolkit.KryptonDateTimePicker datePicker;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox procedureType;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox stateNameTextBox;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel3;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel4;
     }
 }
