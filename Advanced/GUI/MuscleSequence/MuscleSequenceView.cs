3b176c2e3f20d83c4fe06cdf670b6b563ccc5f1c
Andrew Piper <threax@users.noreply.github.com>
2009-11-25 16:39:34 +0000

    Readded MuscleSequence Notes and SavedCamera.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@625 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ Advanced/GUI/MuscleSequence/MuscleSequenceView.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Medical.Properties;
+using Engine.Resources;
+using Engine.Saving.XMLSaver;
+using Medical.Muscles;
+
+namespace Medical.GUI
+{
+    public delegate void MuscleSequenceEvent(String sequenceText, String sequenceFile);
+
+    public partial class MuscleSequenceView : UserControl
+    {
+        private Dictionary<String, ListViewGroup> groups = new Dictionary<string, ListViewGroup>();
+        public event MuscleSequenceEvent SequenceActivated;
+        public event MuscleSequenceEvent SequenceSelected;
+
+        public MuscleSequenceView()
+        {
+            InitializeComponent();
+            muscleStateList.ItemActivate += new EventHandler(muscleStateList_ItemActivate);
+            muscleStateList.SelectedIndexChanged += new EventHandler(muscleStateList_SelectedIndexChanged);
+            muscleStateList.Columns.Add("Name", -2, HorizontalAlignment.Left);
+        }
+
+        public void initializeSequences(SimulationScene simScene, String sceneDirectory)
+        {
+            groups.Clear();
+            muscleStateList.Groups.Clear();
+            String sequenceDir = sceneDirectory + "/" + simScene.SequenceDirectory;
+            using (Archive archive = FileSystem.OpenArchive(sequenceDir))
+            {
+                foreach (String directory in archive.listDirectories(sequenceDir, false))
+                {
+                    ListViewGroup group = new ListViewGroup(archive.getFileInfo(directory).Name);
+                    muscleStateList.Groups.Add(group);
+                    foreach (String file in archive.listFiles(directory, false))
+                    {
+                        String fileName = archive.getFileInfo(file).Name;
+                        ListViewItem listViewItem = new ListViewItem(fileName.Substring(0, fileName.Length - 4), group);
+                        listViewItem.Tag = archive.getFullPath(file);
+                        muscleStateList.Items.Add(listViewItem);
+                    }
+                }
+            }
+        }
+
+        public void clearSequences()
+        {
+            muscleStateList.Items.Clear();
+        }
+
+        void muscleStateList_ItemActivate(object sender, EventArgs e)
+        {
+            if (muscleStateList.SelectedItems.Count > 0 && SequenceActivated != null)
+            {
+                ListViewItem selected = muscleStateList.SelectedItems[0];
+                SequenceActivated.Invoke(selected.Text, selected.Tag.ToString());
+            }
+        }
+
+        void muscleStateList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (SequenceSelected != null)
+            {
+                if (muscleStateList.SelectedItems.Count > 0)
+                {
+                    ListViewItem selected = muscleStateList.SelectedItems[0];
+                    SequenceSelected.Invoke(selected.Text, selected.Tag.ToString());
+                }
+                else
+                {
+                    SequenceSelected.Invoke(null, null);
+                }
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (muscleStateList != null && muscleStateList.Columns.Count > 0)
+            {
+                muscleStateList.Columns[0].Width = -2;
+            }
+        }
+    }
+}
---
+++ Advanced/GUI/SavedCamera/SavedCameraGUI.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+using Engine.ObjectManagement;
+using Medical.Controller;
+
+namespace Medical.GUI
+{
+    public partial class SavedCameraGUI : GUIElement
+    {
+        private ListViewGroup predefined = new ListViewGroup("Predefined");
+        private ListViewGroup userDefined = new ListViewGroup("User Defined");
+        private SavedCameraController userCameras;
+        private DrawingWindowController windowController;
+        private NavigationController navigation;
+        private bool allowCustomCameras = true;
+
+        public SavedCameraGUI()
+        {
+            InitializeComponent();
+            cameraNameList.MouseDoubleClick += new MouseEventHandler(cameraNameList_MouseDoubleClick);
+            cameraNameList.KeyUp += new KeyEventHandler(cameraNameList_KeyUp);
+            cameraNameList.Groups.Add(predefined);
+            cameraNameList.Groups.Add(userDefined);
+        }
+
+        public void initialize(DrawingWindowController windowController, String camerasFile, NavigationController navigation)
+        {
+            this.windowController = windowController;
+            userCameras = new SavedCameraController(camerasFile);
+            if (navigation != null)
+            {
+                this.navigation = navigation;
+                navigation.NavigationStateSetChanged += navigation_NavigationStateSetChanged;
+            }
+        }
+
+        public void createShortcuts(ShortcutController shortcutController)
+        {
+            ShortcutGroup group = shortcutController.createOrRetrieveGroup("Cameras");
+            group.clearShortcuts();
+            bool foundShortcut = false;
+            foreach (ListViewItem item in predefined.Items)
+            {
+                NavigationState state = item.Tag as NavigationState;
+                if (state.ShortcutKey != KeyCodes.None)
+                {
+                    ShortcutEventCommand shortcutEvent = new ShortcutEventCommand(state.Name, (Keys)state.ShortcutKey, true);
+                    shortcutEvent.UserData = state.Name;
+                    shortcutEvent.Execute += shortcutEvent_Execute;
+                    group.addShortcut(shortcutEvent);
+                    item.SubItems.Add("Ctrl + " + state.ShortcutKey.ToString());
+                    foundShortcut = true;
+                }
+            }
+            shortcutColumn.Width = foundShortcut ? 60 : 0;
+        }
+
+        void shortcutEvent_Execute(ShortcutEventCommand shortcut)
+        {
+            navigation.setNavigationState(shortcut.UserData.ToString(), windowController.getActiveWindow().DrawingWindow);
+        }
+
+        public bool AllowCustomCameras
+        {
+            get
+            {
+                return allowCustomCameras;
+            }
+            set
+            {
+                allowCustomCameras = value;
+                saveCameraButton.Visible = value;
+                deleteCameraButton.Visible = value;
+            }
+        }
+
+        protected override void sceneLoaded(SimScene scene)
+        {
+            if (allowCustomCameras)
+            {
+                foreach (String name in userCameras.getSavedCameraNames())
+                {
+                    ListViewItem item = cameraNameList.Items.Add(name, name, 0);
+                    item.Group = userDefined;
+                }
+            }
+            base.sceneLoaded(scene);
+        }
+
+        protected override void sceneUnloading()
+        {
+            cameraNameList.Items.Clear();
+            base.sceneUnloading();
+        }
+
+        void navigation_NavigationStateSetChanged(NavigationController controller)
+        {
+            foreach (NavigationState state in controller.NavigationSet.States)
+            {
+                if (!state.Hidden)
+                {
+                    ListViewItem item = cameraNameList.Items.Add(state.Name, state.Name, 0);
+                    item.Group = predefined;
+                    item.Tag = state;
+                }
+            }
+        }
+
+        private void activateButton_Click(object sender, EventArgs e)
+        {
+            activateSelectedCamera();
+        }
+
+        private void deleteCameraButton_Click(object sender, EventArgs e)
+        {
+            deleteSelectedCamera();
+        }
+
+        private bool validateSaveCameraName(string input, out string error)
+        {
+            if (input == null || input == "")
+            {
+                error = "Please enter a non empty name.";
+                return false;
+            }
+            if (cameraNameList.Items.ContainsKey(input))
+            {
+                error = "Camera name already exists please enter another.";
+                return false;
+            }
+            error = "";
+            return true;
+        }
+
+        void cameraNameList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            activateSelectedCamera();
+        }
+
+        void cameraNameList_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                deleteSelectedCamera();
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                activateSelectedCamera();
+            }
+        }
+
+        private void activateSelectedCamera()
+        {
+            if (cameraNameList.SelectedItems.Count > 0)
+            {
+                ListViewItem item = cameraNameList.SelectedItems[0];
+                if (item.Group == predefined)
+                {
+                    navigation.setNavigationState(item.Text, windowController.getActiveWindow().DrawingWindow);
+                }
+                else
+                {
+                    SavedCameraDefinition def = userCameras.getSavedCamera(item.Text);
+                    windowController.getActiveWindow().DrawingWindow.setCamera(def.Position, def.LookAt);
+                }
+            }
+        }
+
+        private void saveCameraButton_Click(object sender, EventArgs e)
+        {
+            InputResult result = InputBox.GetInput("Save Camera", "Enter a name for the saved camera.", this.FindForm(), validateSaveCameraName);
+            if (result.ok)
+            {
+                DrawingWindowHost currentWindow = windowController.getActiveWindow();
+                userCameras.addOrUpdateSavedCamera(new SavedCameraDefinition(result.text, currentWindow.DrawingWindow.Translation, currentWindow.DrawingWindow.LookAt));
+                if (!cameraNameList.Items.ContainsKey(result.text))
+                {
+                    ListViewItem item = cameraNameList.Items.Add(result.text, result.text, 0);
+                    item.Group = userDefined;
+                }
+                userCameras.saveCameras();
+            }
+        }
+
+        private void deleteSelectedCamera()
+        {
+            if (cameraNameList.SelectedItems.Count > 0)
+            {
+                String selectedItem = cameraNameList.SelectedItems[0].Text;
+                if (userCameras.removeSavedCamera(selectedItem))
+                {
+                    cameraNameList.Items.RemoveByKey(selectedItem);
+                }
+                userCameras.saveCameras();
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (cameraNameList != null)
+            {
+                cameraNameList.Columns[0].Width = -2;
+            }
+        }
+    }
+}
