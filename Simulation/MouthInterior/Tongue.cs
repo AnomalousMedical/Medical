d6c6e7a3d38ad8e85e0c04043ab15cbe252b1662
Andrew Piper <threax@users.noreply.github.com>
2010-03-23 20:23:43 +0000

    Made some tongue behavior classes. The first will run an animation sequence on the tongue. The second will move the collision shapes attached to the tongue with the bones inside.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@936 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ Simulation/MouthInterior/Tongue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine;
+using Engine.Platform;
+using Engine.Editing;
+using OgreWrapper;
+using Engine.ObjectManagement;
+using OgrePlugin;
+using Engine.Attributes;
+using Logging;
+
+namespace Medical
+{
+    public class Tongue : Behavior
+    {
+        [Editable]
+        private String tongueSimObjectName = "Tongue";
+
+        [Editable]
+        private String tongueNodeName = "Node";
+
+        [Editable]
+        private String tongueEntityName = "Entity";
+
+        [Editable]
+        private String protrudeAnimationName = "Protrude";
+
+        AnimationState anim;
+
+        protected override void constructed()
+        {
+            SimObject tongueObject = Owner.getOtherSimObject(tongueSimObjectName);
+            if (tongueObject == null)
+            {
+                blacklist("Could not find Target tongue SimObject {0}.", tongueSimObjectName);
+            }
+
+            SceneNodeElement node = tongueObject.getElement(tongueNodeName) as SceneNodeElement;
+            if (node == null)
+            {
+                blacklist("Could not find target tongue SceneNodeElement {0}.", tongueSimObjectName);
+            }
+
+            Entity entity = node.getNodeObject(tongueEntityName) as Entity;
+            if (entity == null)
+            {
+                blacklist("Could not find tongue Entity {0}.", tongueNodeName);
+            }
+
+            if (!entity.hasSkeleton())
+            {
+                blacklist("Tongue Entity {0} does not have a skeleton.", tongueEntityName);
+            }
+
+            anim = entity.getAnimationState(protrudeAnimationName);
+            if (anim == null)
+            {
+                blacklist("Tongue Entity {0} does not have an animation for protrusion named {1}.", tongueEntityName, protrudeAnimationName);
+            }
+            anim.setLoop(true);
+            anim.setEnabled(true);
+        }
+
+        public override void update(Clock clock, EventManager eventManager)
+        {
+            anim.addTime((float)clock.Seconds / 3.0f);
+        }
+    }
+}
---
+++ Simulation/MouthInterior/TongueCollisionSection.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine;
+using Engine.Editing;
+using Engine.Platform;
+using Engine.ObjectManagement;
+using OgreWrapper;
+using OgrePlugin;
+using Engine.Attributes;
+using BulletPlugin;
+
+namespace Medical
+{
+    public class TongueCollisionSection : Behavior
+    {
+        [Editable]
+        private String tongueSimObjectName = "Tongue";
+
+        [Editable]
+        private String tongueNodeName = "Node";
+
+        [Editable]
+        private String tongueEntityName = "Entity";
+
+        [Editable]
+        private String boneName = "TongueBone5a";
+
+        [Editable]
+        private String jointName = "Joint";
+
+        private Bone bone;
+        protected Generic6DofConstraintElement joint;
+        SimObject tongueObject;
+
+        [DoNotSave]
+        [DoNotCopy]
+        Vector3 offset;
+
+        [DoNotSave]
+        [DoNotCopy]
+        Vector3 lastPosition;
+
+        protected override void constructed()
+        {
+            tongueObject = Owner.getOtherSimObject(tongueSimObjectName);
+            if (tongueObject == null)
+            {
+                blacklist("Could not find Target tongue SimObject {0}.", tongueSimObjectName);
+            }
+
+            SceneNodeElement node = tongueObject.getElement(tongueNodeName) as SceneNodeElement;
+            if (node == null)
+            {
+                blacklist("Could not find target tongue SceneNodeElement {0}.", tongueSimObjectName);
+            }
+
+            Entity entity = node.getNodeObject(tongueEntityName) as Entity;
+            if (entity == null)
+            {
+                blacklist("Could not find tongue Entity {0}.", tongueNodeName);
+            }
+            if (!entity.hasSkeleton())
+            {
+                blacklist("Tongue Entity {0} does not have a skeleton.", tongueEntityName);
+            }
+
+            SkeletonInstance skeleton = entity.getSkeleton();
+            bone = skeleton.getBone(boneName);
+            if (bone == null)
+            {
+                blacklist("Tongue Entity {0} does not have a bone named {1}.", tongueEntityName, boneName);
+            }
+
+            offset = Owner.Translation - tongueObject.Translation - bone.getDerivedPosition();
+            //calculate the scale at 100%
+            Vector3 adjust = (Vector3.ScaleIdentity - Owner.Scale) * offset;
+            offset += adjust;
+
+            joint = Owner.getElement(jointName) as Generic6DofConstraintElement;
+            if (joint == null)
+            {
+                blacklist("Could not find Joint {0}.", jointName);
+            }
+        }
+
+        public override void update(Clock clock, EventManager eventManager)
+        {
+            Vector3 bonePos = bone.getDerivedPosition();
+            if (bonePos != lastPosition)
+            {
+                Vector3 jointPos = tongueObject.Translation + bonePos + offset * Owner.Scale - joint.RigidBodyA.Owner.Translation;
+                joint.setFrameOffsetA(jointPos);
+                lastPosition = bonePos;
+            }
+        }
+    }
+}
