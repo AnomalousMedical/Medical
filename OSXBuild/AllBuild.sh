713a21db19040c2977bf29f41277c543dab1fb38
Andrew Piper <threax@users.noreply.github.com>
2010-07-02 11:57:24 +0000

    Added the OSXBuild directory to the Medical project.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1031 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ OSXBuild/AllBuild.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+rm -r -f Articulometrics.app
+
+#Do the macpack
+macpack -n Articulometrics -a DotNetAssemblies/Standalone.exe -m cocoa -i Skull.icns
+
+#Copy the .net assemblies
+cp DotNetAssemblies/*.dll Articulometrics.app/Contents/Resources
+cp DotNetAssemblies/Standalone.exe Articulometrics.app/Contents/Resources/Articulometrics.exe
+
+#Copy the mac native dylibs
+cp ../../Engine/OSXBuild/Release/*.dylib Articulometrics.app/Contents/Resources
+
+#Copy the frameworks
+mkdir Articulometrics.app/Contents/Frameworks
+mkdir Articulometrics.app/Contents/Frameworks/Cg.framework
+mkdir Articulometrics.app/Contents/Frameworks/Ogre.framework
+mkdir Articulometrics.app/Contents/Frameworks/Ogre.framework/Versions
+mkdir Articulometrics.app/Contents/Frameworks/Ogre.framework/Versions/1.7.1
+
+cp ../../Dependencies/Ogre/OSXBuild/lib/Release/*.dylib Articulometrics.app/Contents/Resources
+cp ../../Dependencies/Ogre/OSXBuild/lib/Release/Ogre.framework/Versions/1.7.1/Ogre Articulometrics.app/Contents/Frameworks/Ogre.framework/Versions/1.7.1/Ogre
+cp ../../Dependencies/Ogre/OSXBuild/Dependencies/Cg.framework/Versions/1.0/Cg Articulometrics.app/Contents/Frameworks/Cg.framework/Cg
+
+#Create dummy files
+mkdir Articulometrics.app/Contents/Resources/bin
+mkdir Articulometrics.app/Contents/Resources/bin/Contents
+mkdir Articulometrics.app/Contents/Resources/bin/Contents/Plugins
+
+#Copy override
+cp override.ini Articulometrics.app/Contents/Resources
