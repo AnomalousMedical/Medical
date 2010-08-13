ec985f12a72f3dc93465bf6c37eaf78f15a59a86
Andrew Piper <threax@users.noreply.github.com>
2010-08-13 19:16:25 +0000

    Added the version to the AssemblyInfo.cs using subwcrev.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1138 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Standalone/Properties/AssemblyInfoSource.cs
+++ Standalone/Properties/AssemblyInfoSource.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Standalone")]
+[assembly: AssemblyTitle("Piper JBO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Standalone")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("Anomalous Medical")]
+[assembly: AssemblyProduct("Piper JBO")]
+[assembly: AssemblyCopyright("Copyright © Anomalous Medical 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.$WCREV$")]
+[assembly: AssemblyFileVersion("2.0.0.$WCREV$")]
