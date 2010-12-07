fadd7fd41aa3c6e288a43b9af4b2b8644a7cbc5b
Andrew Piper <threax@users.noreply.github.com>
2010-12-07 21:21:37 +0000

    Starting to get the question dialog working.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1435 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Standalone/GUI/Timeline/QuestionProvider/MyGUIQuestionProvider.cs
+++ Standalone/GUI/Timeline/QuestionProvider/MyGUIQuestionProvider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MyGUIPlugin;
+using Engine;
 
 namespace Medical.GUI
 {
@@ -13,17 +14,20 @@ namespace Medical.GUI
         public MyGUIQuestionProvider()
             :base("Medical.GUI.Timeline.QuestionProvider.MyGUIQuestionProvider.layout")
         {
-
+            questionScroll = window.findWidget("QuestionScroll") as ScrollView;
         }
 
         public void addQuestion(PromptQuestion question)
         {
-            
+            PromptTextArea textArea = new PromptTextArea(question.Text, questionScroll, window.Width);
+
+            Size2 canvasSize = questionScroll.CanvasSize;
+            questionScroll.CanvasSize = new Size2(window.Width, textArea.Bottom);
         }
 
         public void showPrompt(PromptAnswerSelected answerSelectedCallback)
         {
-            
+            this.open(true);
         }
 
         public void clear()
---
+++ Standalone/GUI/Timeline/QuestionProvider/PromptTextArea.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyGUIPlugin;
+using Engine;
+
+namespace Medical.GUI
+{
+    class PromptTextArea
+    {
+        private const int EXTRA_HEIGHT = 5;
+
+        private ScrollView parent;
+        private String text;
+        private Edit questionEdit;
+
+        public PromptTextArea(String text, ScrollView parent, int questionWidth)
+        {
+            this.parent = parent;
+            this.text = text;
+
+            questionEdit = parent.createWidgetT("Edit", "WordWrapSimple", 0, 0, questionWidth, 0, Align.Stretch, "") as Edit;
+            questionEdit.Caption = text;
+            questionEdit.EditReadOnly = true;
+            Size2 textSize = questionEdit.getTextSize();
+            questionEdit.setSize(questionWidth, (int)textSize.Height + EXTRA_HEIGHT);
+        }
+
+        public int Bottom
+        {
+            get
+            {
+                return questionEdit.Top + questionEdit.Height;
+            }
+        }
+    }
+}
