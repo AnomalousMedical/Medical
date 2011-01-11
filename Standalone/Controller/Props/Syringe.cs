bee8f009044bd1baccb7b099e7cbb019d92fda5e
Andrew Piper <threax@users.noreply.github.com>
2011-01-11 16:44:27 +0000

    Added some behavior for the syringe.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1537 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Standalone/Controller/Props/Syringe.cs
+++ Standalone/Controller/Props/Syringe.cs
@@ -5,11 +5,27 @@ using System.Text;
 using Engine.ObjectManagement;
 using OgrePlugin;
 using Engine;
+using Engine.Platform;
+using Engine.Editing;
+using OgreWrapper;
+using Engine.Attributes;
 
 namespace Medical
 {
-    class Syringe
+    class Syringe : Behavior
     {
+        [Editable] private String nodeName = PropFactory.NodeName;
+        [Editable] private String entityName = PropFactory.EntityName;
+        [Editable] private String plungerBoneName = "BonePlunger";
+        [Editable] private float totalPlungeRange = 4.3f;
+
+        [DoNotCopy][DoNotSave] private bool doPlunge = false;
+        [DoNotCopy][DoNotSave] private float plungeDuration = 1.0f;
+        [DoNotCopy][DoNotSave] private float plungeRangePercent = 1.0f;
+        private Bone plungerBone;
+        [DoNotCopy][DoNotSave] private Vector3 plungerBoneStart;
+        [DoNotCopy][DoNotSave] private float timeCounter = 0.0f;
+
         public static void createPropDefinition(PropFactory propFactory)
         {
             GenericSimObjectDefinition syringe = new GenericSimObjectDefinition("Syringe");
@@ -22,7 +38,68 @@ namespace Medical
             PropFadeBehavior propFadeBehavior = new PropFadeBehavior();
             BehaviorDefinition propFadeBehaviorDef = new BehaviorDefinition(PropFactory.FadeBehaviorName, propFadeBehavior);
             syringe.addElement(propFadeBehaviorDef);
+            BehaviorDefinition syringeBehavior = new BehaviorDefinition("Behavior", new Syringe());
+            syringe.addElement(syringeBehavior);
             propFactory.addDefinition("Syringe", syringe);
+        }
+
+        protected override void constructed()
+        {
+            base.constructed();
+            SceneNodeElement sceneNode = Owner.getElement(nodeName) as SceneNodeElement;
+            if (sceneNode == null)
+            {
+                blacklist("Could not find SceneNode {0} in Syringe SimObject", nodeName);
+            }
+            Entity entity = sceneNode.getNodeObject(entityName) as Entity;
+            if (entity == null)
+            {
+                blacklist("Could not find Entity {0} in Syringe SimObject", entityName);
+            }
+            if (entity.hasSkeleton())
+            {
+                SkeletonInstance skeleton = entity.getSkeleton();
+                if (skeleton.hasBone(plungerBoneName))
+                {
+                    plungerBone = skeleton.getBone(plungerBoneName);
+                    plungerBone.setManuallyControlled(true);
+                    plungerBoneStart = plungerBone.getPosition();
+                }
+                else
+                {
+                    blacklist("Could not find plunger bone {0} for syringe", plungerBoneName);
+                }
+            }
+            else
+            {
+                blacklist("Entity has no skeleton in Syringe SimObject");
+            }
+        }
+
+        public override void update(Clock clock, EventManager eventManager)
+        {
+            if (doPlunge)
+            {
+                timeCounter += clock.fSeconds;
+                float boneInterpolate = (timeCounter) / plungeDuration;
+                if (boneInterpolate > plungeRangePercent)
+                {
+                    boneInterpolate = plungeRangePercent;
+                    doPlunge = false;
+                }
+                Vector3 newPos = plungerBoneStart;
+                newPos.y -= (totalPlungeRange * boneInterpolate);
+                plungerBone.setPosition(newPos);
+                plungerBone.needUpdate(true);
+            }
+        }
+
+        public void plunge(float percent, float duration)
+        {
+            plungeRangePercent = percent;
+            plungeDuration = duration;
+            doPlunge = true;
+            timeCounter = 0.0f;
         }
     }
 }
