578910f529846df97c02c99dd3feab3a1461ed8d
Andrew Piper <threax@users.noreply.github.com>
2011-01-11 21:53:54 +0000

    The PushPlungerProperties are now functional.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1545 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Editor/GUI/PropTimeline/SubActionProperties/PushPlungerProperties.cs
+++ Editor/GUI/PropTimeline/SubActionProperties/PushPlungerProperties.cs
@@ -8,15 +8,29 @@ namespace Medical.GUI
 {
     class PushPlungerProperties : TimelineDataPanel
     {
+        private NumericEdit pushPlungerEdit;
+        private PushPlungerAction pushPlunger;
+
         public PushPlungerProperties(Widget parentWidget)
             :base(parentWidget, "Medical.GUI.PropTimeline.SubActionProperties.PushPlungerProperties.layout")
         {
-
+            pushPlungerEdit = new NumericEdit(mainWidget.findWidget("PlungeAmountEdit") as Edit);
+            pushPlungerEdit.MinValue = 0.0f;
+            pushPlungerEdit.MaxValue = 1.0f;
+            pushPlungerEdit.Increment = 0.1f;
+            pushPlungerEdit.ValueChanged += new MyGUIEvent(pushPlungerEdit_ValueChanged);
         }
 
         public override void setCurrentData(TimelineData data)
         {
-            
+            PropTimelineData propData = (PropTimelineData)data;
+            pushPlunger = (PushPlungerAction)propData.Action;
+            pushPlungerEdit.FloatValue = pushPlunger.PlungePercentage;
+        }
+
+        void pushPlungerEdit_ValueChanged(Widget source, EventArgs e)
+        {
+            pushPlunger.PlungePercentage = pushPlungerEdit.FloatValue;
         }
     }
 }
