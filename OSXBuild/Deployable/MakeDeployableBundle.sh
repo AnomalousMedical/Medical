81e8a9ac5fb7ff29bfa8080b12208c6eb0876717
Andrew Piper <threax@users.noreply.github.com>
2011-01-19 18:51:01 +0000

    Modified the deployable bundle to include all needed files.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1580 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- OSXBuild/Deployable/MakeDeployableBundle.sh
+++ OSXBuild/Deployable/MakeDeployableBundle.sh
@@ -6,17 +6,21 @@
 # Created by Andrew Piper on 1/18/11.
 # Copyright 2011 Anomalous Software. All rights reserved.
 
+#Discover some stuff about where we are.
+START_PATH=$(pwd)
 
 APP_NAME="PiperJBO"
 BUNDLE_SOURCE_DIR="BundleSource"
 BUILD_DIR="Build"
-DOT_NET_EXECUTABLES="../DotNetAssemblies"
 APP_BUNDLE_NAME="$APP_NAME.app"
-MONO_FRAMEWORK_HOME="/Users/andrewpiper/Development/Dependencies/Mono/OSXBuild"
+DEVELOPMENT_HOME="/Users/andrewpiper/Development"
+MONO_FRAMEWORK_HOME="$DEVELOPMENT_HOME/Dependencies/Mono/OSXBuild"
+ENGINE_HOME="$DEVELOPMENT_HOME/Engine"
+MEDICAL_HOME="$DEVELOPMENT_HOME/Medical"
+DEPENDENCIES_HOME="$DEVELOPMENT_HOME/Dependencies"
 
-#Discover some stuff about where we are.
-START_PATH=$(pwd)
-MAIN_EXE="$START_PATH/$DOT_NET_EXECUTABLES/$APP_NAME.exe"
+DOT_NET_EXECUTABLES="$MEDICAL_HOME/OSXBuild/DotNetAssemblies"
+MAIN_EXE="$DOT_NET_EXECUTABLES/$APP_NAME.exe"
 
 #Make sure the temp directories are empty by removing them
 sudo rm -r "$APP_BUNDLE_NAME"
@@ -24,13 +28,38 @@ sudo rm -r "$APP_BUNDLE_NAME"
 #Create the app bundle
 mkdir "$APP_BUNDLE_NAME"
 cp -r "$BUNDLE_SOURCE_DIR/Contents" "$APP_BUNDLE_NAME"
-cp "$MAIN_EXE" "$APP_BUNDLE_NAME/Contents/Resources"
-chmod 755 "$APP_BUNDLE_NAME/Contents/MacOS/PiperJBO"
+chmod 755 "$APP_BUNDLE_NAME/Contents/MacOS/$APP_NAME"
 rm -rf `find $APP_BUNDLE_NAME -type d -name .svn`
 mkdir "$APP_BUNDLE_NAME/Contents/Frameworks"
 
+#Copy program files
+cp "$MAIN_EXE" "$APP_BUNDLE_NAME/Contents/Resources"
+cp "$DOT_NET_EXECUTABLES/"*.dll "$APP_BUNDLE_NAME/Contents/Resources"
+
+#Copy the mac native dylibs
+cp "$ENGINE_HOME/OSXBuild/Release/"*.dylib "$APP_BUNDLE_NAME/Contents/Resources"
+
+#Copy the Medical native dylibs
+cp "$MEDICAL_HOME/OSXBuild/OSHelper/build/Release/libOSHelper.dylib" "$APP_BUNDLE_NAME/Contents/Resources"
+
+#Copy the wx.NET native dylibs
+cp "$DEPENDENCIES_HOME/wxDotNet/Build/OSXBuild/wxDotNet/build/Release/libwx-c.dylib" "$APP_BUNDLE_NAME/Contents/Resources"
+
+#Copy the ogre frameworks
+mkdir "$APP_BUNDLE_NAME/Contents/Frameworks/Cg.framework"
+mkdir "$APP_BUNDLE_NAME/Contents/Frameworks/Ogre.framework"
+mkdir "$APP_BUNDLE_NAME/Contents/Frameworks/Ogre.framework/Versions"
+mkdir "$APP_BUNDLE_NAME/Contents/Frameworks/Ogre.framework/Versions/1.7.1"
+
+cp "$DEPENDENCIES_HOME/Ogre/OSXBuild/lib/Release/"*.dylib "$APP_BUNDLE_NAME/Contents/Resources"
+cp "$DEPENDENCIES_HOME/Ogre/OSXBuild/lib/Release/Ogre.framework/Versions/1.7.1/Ogre" "$APP_BUNDLE_NAME/Contents/Frameworks/Ogre.framework/Versions/1.7.1/Ogre"
+cp "$DEPENDENCIES_HOME/Ogre/OSXBuild/Dependencies/Cg.framework/Versions/1.0/Cg" "$APP_BUNDLE_NAME/Contents/Frameworks/Cg.framework/Cg"
+
 #Copy mono framework
 cp -r "$MONO_FRAMEWORK_HOME/Mono.framework" "$APP_BUNDLE_NAME/Contents/Frameworks/Mono.framework"
+
+#Copy override
+cp ../override.ini "$APP_BUNDLE_NAME/Contents/Resources"
 
 #Finish up
 cd "$START_PATH"
