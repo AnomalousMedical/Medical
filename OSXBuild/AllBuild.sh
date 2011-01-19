a5b8eb6f3efccebf75ea845e700c35bf5f2cb8b5
Andrew Piper <threax@users.noreply.github.com>
2011-01-19 16:35:42 +0000

    Modified AllBuild.sh to build something called PiperJBO. Modified the deployment build to use the standalone mono being created instead of the mkbundle method.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1579 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- OSXBuild/AllBuild.sh
+++ OSXBuild/AllBuild.sh
@@ -1,42 +1,42 @@
 #!/bin/sh
 
 #Delete old app
-rm -r -f Articulometrics.app
+rm -r -f "Piper JBO.app"
 
 #Compile native libs
 xcodebuild -project OSHelper/OSHelper.xcodeproj -alltargets
 
 #Do the macpack
-macpack -n Articulometrics -a DotNetAssemblies/PiperJBO.exe -m cocoa -i Skull.icns
+macpack -n "Piper JBO" -a DotNetAssemblies/PiperJBO.exe -m cocoa -i Skull.icns
 
 #Copy the .net assemblies
-cp DotNetAssemblies/*.dll Articulometrics.app/Contents/Resources
-cp DotNetAssemblies/PiperJBO.exe Articulometrics.app/Contents/Resources/Articulometrics.exe
+cp DotNetAssemblies/*.dll "Piper JBO.app/Contents/Resources"
+cp DotNetAssemblies/PiperJBO.exe "Piper JBO.app/Contents/Resources/Piper JBO.exe"
 
 #Copy the mac native dylibs
-cp ../../Engine/OSXBuild/Release/*.dylib Articulometrics.app/Contents/Resources
+cp ../../Engine/OSXBuild/Release/*.dylib "Piper JBO.app/Contents/Resources"
 
 #Copy the Medical native dylibs
-cp OSHelper/build/Release/libOSHelper.dylib Articulometrics.app/Contents/Resources
+cp OSHelper/build/Release/libOSHelper.dylib "Piper JBO.app/Contents/Resources"
 
 #Copy the wx.NET native dylibs
-cp ../../Dependencies/wxDotNet/Build/OSXBuild/wxDotNet/build/Release/libwx-c.dylib Articulometrics.app/Contents/Resources
+cp ../../Dependencies/wxDotNet/Build/OSXBuild/wxDotNet/build/Release/libwx-c.dylib "Piper JBO.app/Contents/Resources"
 
 #Copy the frameworks
-mkdir Articulometrics.app/Contents/Frameworks
-mkdir Articulometrics.app/Contents/Frameworks/Cg.framework
-mkdir Articulometrics.app/Contents/Frameworks/Ogre.framework
-mkdir Articulometrics.app/Contents/Frameworks/Ogre.framework/Versions
-mkdir Articulometrics.app/Contents/Frameworks/Ogre.framework/Versions/1.7.1
+mkdir "Piper JBO.app/Contents/Frameworks"
+mkdir "Piper JBO.app/Contents/Frameworks/Cg.framework"
+mkdir "Piper JBO.app/Contents/Frameworks/Ogre.framework"
+mkdir "Piper JBO.app/Contents/Frameworks/Ogre.framework/Versions"
+mkdir "Piper JBO.app/Contents/Frameworks/Ogre.framework/Versions/1.7.1"
 
-cp ../../Dependencies/Ogre/OSXBuild/lib/Release/*.dylib Articulometrics.app/Contents/Resources
-cp ../../Dependencies/Ogre/OSXBuild/lib/Release/Ogre.framework/Versions/1.7.1/Ogre Articulometrics.app/Contents/Frameworks/Ogre.framework/Versions/1.7.1/Ogre
-cp ../../Dependencies/Ogre/OSXBuild/Dependencies/Cg.framework/Versions/1.0/Cg Articulometrics.app/Contents/Frameworks/Cg.framework/Cg
+cp ../../Dependencies/Ogre/OSXBuild/lib/Release/*.dylib "Piper JBO.app/Contents/Resources"
+cp ../../Dependencies/Ogre/OSXBuild/lib/Release/Ogre.framework/Versions/1.7.1/Ogre "Piper JBO.app/Contents/Frameworks/Ogre.framework/Versions/1.7.1/Ogre"
+cp ../../Dependencies/Ogre/OSXBuild/Dependencies/Cg.framework/Versions/1.0/Cg "Piper JBO.app/Contents/Frameworks/Cg.framework/Cg"
 
 #Create dummy files
-mkdir Articulometrics.app/Contents/Resources/bin
-mkdir Articulometrics.app/Contents/Resources/bin/Contents
-mkdir Articulometrics.app/Contents/Resources/bin/Contents/Plugins
+mkdir "Piper JBO.app/Contents/Resources/bin"
+mkdir "Piper JBO.app/Contents/Resources/bin/Contents"
+mkdir "Piper JBO.app/Contents/Resources/bin/Contents/Plugins"
 
 #Copy override
-cp override.ini Articulometrics.app/Contents/Resources
+cp override.ini "Piper JBO.app/Contents/Resources"
--- OSXBuild/Deployable/MakeDeployableBundle.sh
+++ OSXBuild/Deployable/MakeDeployableBundle.sh
@@ -12,41 +12,25 @@ BUNDLE_SOURCE_DIR="BundleSource"
 BUILD_DIR="Build"
 DOT_NET_EXECUTABLES="../DotNetAssemblies"
 APP_BUNDLE_NAME="$APP_NAME.app"
+MONO_FRAMEWORK_HOME="/Users/andrewpiper/Development/Dependencies/Mono/OSXBuild"
 
 #Discover some stuff about where we are.
 START_PATH=$(pwd)
 MAIN_EXE="$START_PATH/$DOT_NET_EXECUTABLES/$APP_NAME.exe"
 
 #Make sure the temp directories are empty by removing them
-rm -r "$BUILD_DIR"
 sudo rm -r "$APP_BUNDLE_NAME"
-
-#Setup mkbundle environment vars
-export PKG_CONFIG_PATH="/Library/Frameworks/Mono.framework/Versions/Current/lib/pkgconfig/"
-export AS="as -arch i386"
-export CC="cc -arch i386"
-PATH=$PATH:"/Library/Frameworks/Mono.framework/Versions/Current/bin"
-
-#Link with mkbundle
-mkdir "$BUILD_DIR"
-cd "$BUILD_DIR"
-mkbundle "$MAIN_EXE" -L "." -c -o "$APP_NAME.c" --keeptemp
-cc -arch i386 -g -o "$APP_NAME" -Wall "$APP_NAME.c" `pkg-config --cflags --libs mono-2` temp.o -framework CoreFoundation
-cd "$START_PATH"
 
 #Create the app bundle
 mkdir "$APP_BUNDLE_NAME"
 cp -r "$BUNDLE_SOURCE_DIR/Contents" "$APP_BUNDLE_NAME"
-cp "$BUILD_DIR/$APP_NAME" "$APP_BUNDLE_NAME/Contents/Resources"
+cp "$MAIN_EXE" "$APP_BUNDLE_NAME/Contents/Resources"
 chmod 755 "$APP_BUNDLE_NAME/Contents/MacOS/PiperJBO"
 rm -rf `find $APP_BUNDLE_NAME -type d -name .svn`
+mkdir "$APP_BUNDLE_NAME/Contents/Frameworks"
 
-#Fix library paths with nant script
-sudo nant -buildfile:fixmonopaths.build -D:arg.programName="$APP_NAME"
-
-#Copy mono files as needed
-sudo cp "/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/2.0/mscorlib.dll" "$APP_BUNDLE_NAME/Contents/Frameworks/Mono/lib"
+#Copy mono framework
+cp -r "$MONO_FRAMEWORK_HOME/Mono.framework" "$APP_BUNDLE_NAME/Contents/Frameworks/Mono.framework"
 
 #Finish up
 cd "$START_PATH"
-rm -r "$BUILD_DIR"
