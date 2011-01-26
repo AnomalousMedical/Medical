98744a282029c04b2f3d29e7beef7d046daccc0b
Andrew Piper <threax@users.noreply.github.com>
2011-01-26 16:25:59 +0000

    Implemented the native side of the SystemInfo class.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1604 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ OSHelper/SystemInfo.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include <wx/display.h>
+
+extern "C" _AnomalousExport uint SystemInfo_getDisplayCount()
+{
+	return wxDisplay::GetCount();
+}
+
+extern "C" _AnomalousExport void SystemInfo_getDisplayLocation(int displayIndex, int& x, int& y)
+{
+	wxDisplay display(displayIndex);
+	wxRect& geometry = display.GetGeometry();
+	x = geometry.x;
+	y = geometry.y;
+}
--- Standalone/Platform/SystemInfo.cs
+++ Standalone/Platform/SystemInfo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace Medical
 {
@@ -14,13 +15,25 @@ namespace Medical
         {
             get
             {
-                return 1;
+                return SystemInfo_getDisplayCount();
             }
         }
 
         public static Point getDisplayLocation(int displayIndex)
         {
-            return new Point(-1, -1);
+            int x, y;
+            SystemInfo_getDisplayLocation(displayIndex, out x, out y);
+            return new Point(x, y);
         }
+
+        #region PInvoke
+
+        [DllImport("OSHelper")]
+        private static extern uint SystemInfo_getDisplayCount();
+
+        [DllImport("OSHelper")]
+        private static extern void SystemInfo_getDisplayLocation(int displayIndex, out int x, out int y);
+
+        #endregion
     }
 }
