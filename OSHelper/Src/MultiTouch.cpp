ae677681810e2987ef07cdd59f01229920f2c5c6
Andrew Piper <threax@users.noreply.github.com>
2011-01-27 21:51:30 +0000

    Got some more multitouch working. Got a nice #define system going.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1620 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- OSHelper/Src/MultiTouch.cpp
+++ OSHelper/Src/MultiTouch.cpp
@@ -1,23 +1,14 @@
 #include "stdafx.h"
 
+//#define MULTITOUCH
+
+#ifdef MULTITOUCH
+
 #ifdef WINDOWS
 
 #ifndef WINVER                  // Specifies that the minimum required platform is Windows 7.
 #define WINVER 0x0601           // Change this to the appropriate value to target other versions of Windows.
 #endif
-
-#ifndef _WIN32_WINNT            // Specifies that the minimum required platform is Windows 7.
-#define _WIN32_WINNT 0x0601     // Change this to the appropriate value to target other versions of Windows.
-#endif     
-
-#ifndef _WIN32_WINDOWS          // Specifies that the minimum required platform is Windows 98.
-#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
-#endif
-
-#ifndef _WIN32_IE                       // Specifies that the minimum required platform is Internet Explorer 7.0.
-#define _WIN32_IE 0x0700        // Change this to the appropriate value to target other versions of IE.
-#endif
-
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
@@ -32,49 +23,78 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 WndFunc newWndFunc = WndProc;
 WndFunc oldWndFunc;
 
-//int wmId, wmEvent, i, x, y;
-//
-//UINT cInputs;
-//PTOUCHINPUT pInputs;
-//POINT ptInput;  
+int wmId, wmEvent, i, x, y;
+
+UINT cInputs;
+PTOUCHINPUT pInputs;
+POINT ptInput;  
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	//switch(message)
-	//{
-	//case WM_TOUCH:
-	//	cInputs = LOWORD(wParam);
-	//	pInputs = new TOUCHINPUT[cInputs];
-	//	if (pInputs)
-	//	{
-	//		if (GetTouchInputInfo((HTOUCHINPUT)lParam, cInputs, pInputs, sizeof(TOUCHINPUT)))
-	//		{
-	//			for (int i=0; i < static_cast<INT>(cInputs); i++)
-	//			{
-	//				TOUCHINPUT ti = pInputs[i];
-	//				if(ti.dwFlags & TOUCHEVENTF_UP)
-	//				{
-	//					
-	//				}
-	//			}
-	//		}
-	//		// If you handled the message and don't want anything else done with it, you can close it
-	//		CloseTouchInputHandle((HTOUCHINPUT)lParam);
-	//		delete [] pInputs;
-	//	}else{
-	//		// Handle the error here 
-	//	}  
+	switch(message)
+	{
+	case WM_TOUCH:
+		cInputs = LOWORD(wParam);
+		pInputs = new TOUCHINPUT[cInputs];
+		if (pInputs)
+		{
+			if (GetTouchInputInfo((HTOUCHINPUT)lParam, cInputs, pInputs, sizeof(TOUCHINPUT)))
+			{
+				for (int i=0; i < static_cast<INT>(cInputs); i++)
+				{
+					TOUCHINPUT ti = pInputs[i];
+					if(ti.dwFlags & TOUCHEVENTF_MOVE)
+					{
+						std::cout << "Moved " << ti.dwID << " x " << ti.x << " y " << ti.y << " contactX " << ti.cxContact << " contactY " << ti.cyContact << std::endl;
+					}
+					else if(ti.dwFlags & TOUCHEVENTF_DOWN)
+					{
+						std::cout << "Touched down" << ti.dwID  << std::endl;
+					}
+					else if(ti.dwFlags & TOUCHEVENTF_UP)
+					{
+						std::cout << "Touched up " << ti.dwID << std::endl;
+					}
+					//Don't process these, but leave them for reference
+					/*else if(ti.dwFlags & TOUCHEVENTF_INRANGE)
+					{
+						std::cout << "Touched in range " << ti.dwID << std::endl;
+					}
+					else if(ti.dwFlags & TOUCHEVENTF_PRIMARY)
+					{
+						std::cout << "Touched primary " << ti.dwID << std::endl;
+					}
+					else if(ti.dwFlags & TOUCHEVENTF_NOCOALESCE)
+					{
+						std::cout << "Touched nocoalesce " << ti.dwID << std::endl;
+					}
+					else if(ti.dwFlags & TOUCHEVENTF_PEN)
+					{
+						std::cout << "Touched pen " << ti.dwID << std::endl;
+					}
+					else if(ti.dwFlags & TOUCHEVENTF_PALM)
+					{
+						std::cout << "Touched palm " << ti.dwID << std::endl;
+					}*/
+				}
+			}
+			// If you handled the message and don't want anything else done with it, you can close it
+			CloseTouchInputHandle((HTOUCHINPUT)lParam);
+			delete [] pInputs;
+		}else{
+			// Handle the error here 
+		}  
 
 
-	//	break;
-	//}
+		break;
+	}
 
 	return oldWndFunc(hWnd, message, wParam, lParam);
 }
 
 extern "C" _AnomalousExport void MultiTouch_registerMultiTouchEventHandler(HWND hwnd)
 {
-	//RegisterTouchWindow(hwnd, 0);
+	RegisterTouchWindow(hwnd, 0);
 
 	oldWndFunc = (WndFunc)GetWindowLong(hwnd, GWLP_WNDPROC);
 	long wndProcLong = (long)newWndFunc;
@@ -83,10 +103,10 @@ extern "C" _AnomalousExport void MultiTouch_registerMultiTouchEventHandler(HWND 
 
 extern "C" _AnomalousExport bool MultiTouch_isMultitouchAvailable()
 {
-	return false;
+	return true;
 }
 
-#endif
+#endif //WINDOWS
 
 #ifdef MAC_OSX
 
@@ -100,4 +120,18 @@ extern "C" _AnomalousExport bool MultiTouch_isMultitouchAvailable()
 	return false;
 }
 
-#endif
+#endif //MAC_OSX
+
+#else //Else from MULTITOUCH
+
+extern "C" _AnomalousExport void MultiTouch_registerMultiTouchEventHandler(void* hwnd)
+{
+
+}
+
+extern "C" _AnomalousExport bool MultiTouch_isMultitouchAvailable()
+{
+	return false;
+}
+
+#endif //MULTITOUCH Else
