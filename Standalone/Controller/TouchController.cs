2b25be2184494027e3a69a9a1f223eec1d6fe33e
Andrew Piper <threax@users.noreply.github.com>
2011-01-28 21:31:26 +0000

    Renamed gestures and changed button count depending on the os.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1628 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Standalone/Controller/TouchController.cs
+++ Standalone/Controller/TouchController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Engine.Platform;
 using Medical.Controller;
+using MyGUIPlugin;
 
 namespace Medical
 {
@@ -13,9 +14,17 @@ namespace Medical
         private GestureEngine gestureEngine;
         private UpdateTimer mainTimer;
         private SceneViewController sceneViewController;
-        private MultiFingerScrollGesture oneFingerScroll;
-        private MultiFingerScrollGesture twoFingerScroll;
-        private TwoFingerZoom twoFingerZoom;
+        private MultiFingerScrollGesture rotateGesture;
+        private MultiFingerScrollGesture panGesture;
+        private TwoFingerZoom zoomGesture;
+
+#if WINDOWS
+        private const int ROTATE_FINGER_COUNT = 1;
+        private const int PAN_FINGER_COUNT = 2;
+#elif MAC_OSX
+        private const int ROTATE_FINGER_COUNT = 2;
+        private const int PAN_FINGER_COUNT = 3;
+#endif
 
         public TouchController(OSWindow window, UpdateTimer mainTimer, SceneViewController sceneViewController)
         {
@@ -25,17 +34,17 @@ namespace Medical
             gestureEngine = new GestureEngine(multiTouch);
             mainTimer.addFixedUpdateListener(gestureEngine);
 
-            oneFingerScroll = new MultiFingerScrollGesture(1);
-            oneFingerScroll.Scroll += new MultiFingerScrollGesture.ScrollDelegate(oneFingerScroll_Scroll);
-            gestureEngine.addGesture(oneFingerScroll);
+            rotateGesture = new MultiFingerScrollGesture(ROTATE_FINGER_COUNT);
+            rotateGesture.Scroll += new MultiFingerScrollGesture.ScrollDelegate(rotateGesture_Scroll);
+            gestureEngine.addGesture(rotateGesture);
 
-            twoFingerZoom = new TwoFingerZoom();
-            twoFingerZoom.Zoom += new TwoFingerZoom.ZoomDelegate(twoFingerZoom_Zoom);
-            gestureEngine.addGesture(twoFingerZoom);
+            zoomGesture = new TwoFingerZoom();
+            zoomGesture.Zoom += new TwoFingerZoom.ZoomDelegate(zoomGesture_Zoom);
+            gestureEngine.addGesture(zoomGesture);
 
-            twoFingerScroll = new MultiFingerScrollGesture(2);
-            twoFingerScroll.Scroll += new MultiFingerScrollGesture.ScrollDelegate(twoFingerScroll_Scroll);
-            gestureEngine.addGesture(twoFingerScroll);
+            panGesture = new MultiFingerScrollGesture(PAN_FINGER_COUNT);
+            panGesture.Scroll += new MultiFingerScrollGesture.ScrollDelegate(panGesture_Scroll);
+            gestureEngine.addGesture(panGesture);
         }
 
         public void Dispose()
@@ -44,33 +53,42 @@ namespace Medical
             multiTouch.Dispose();
         }
 
-        void oneFingerScroll_Scroll(float deltaX, float deltaY)
+        void rotateGesture_Scroll(float deltaX, float deltaY)
         {
-            SceneViewWindow sceneView = sceneViewController.ActiveWindow;
-            if (sceneView != null)
+            if (!Gui.Instance.HandledMouseButtons)
             {
-                float sensitivity = 10.0f;
-                sceneView.rotate(-deltaX * sensitivity, deltaY * sensitivity);
+                SceneViewWindow sceneView = sceneViewController.ActiveWindow;
+                if (sceneView != null)
+                {
+                    float sensitivity = 10.0f;
+                    sceneView.rotate(-deltaX * sensitivity, deltaY * sensitivity);
+                }
             }
         }
 
-        void twoFingerScroll_Scroll(float deltaX, float deltaY)
+        void panGesture_Scroll(float deltaX, float deltaY)
         {
-            SceneViewWindow sceneView = sceneViewController.ActiveWindow;
-            if (sceneView != null)
+            if (!Gui.Instance.HandledMouseButtons)
             {
-                float sensitivity = 50.0f;
-                sceneView.pan(deltaX * sensitivity, deltaY * sensitivity);
+                SceneViewWindow sceneView = sceneViewController.ActiveWindow;
+                if (sceneView != null)
+                {
+                    float sensitivity = 50.0f;
+                    sceneView.pan(deltaX * sensitivity, deltaY * sensitivity);
+                }
             }
         }
 
-        void twoFingerZoom_Zoom(float zoomDelta)
+        void zoomGesture_Zoom(float zoomDelta)
         {
-            SceneViewWindow sceneView = sceneViewController.ActiveWindow;
-            if (sceneView != null)
+            if (!Gui.Instance.HandledMouseButtons)
             {
-                float sensitivity = 200.0f;
-                sceneView.zoom(zoomDelta * sensitivity);
+                SceneViewWindow sceneView = sceneViewController.ActiveWindow;
+                if (sceneView != null)
+                {
+                    float sensitivity = 200.0f;
+                    sceneView.zoom(zoomDelta * sensitivity);
+                }
             }
         }
     }
