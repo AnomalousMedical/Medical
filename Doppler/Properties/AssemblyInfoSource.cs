92eb6dd87c5f969a83d8f84b7159c9b9ec8f0988
Andrew Piper <threax@users.noreply.github.com>
2011-02-08 18:47:48 +0000

    Made the doppler create its assembly info with subwcrev like everything else.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1699 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Doppler/Properties/AssemblyInfoSource.cs
+++ Doppler/Properties/AssemblyInfoSource.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Doppler")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Doppler")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Anomalous Medical")]
+[assembly: AssemblyProduct("Doppler Diagnostic Module by Dr. Mark Piper")]
+[assembly: AssemblyCopyright("Copyright © Anomalous Medical 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.$WCREV$")]
+[assembly: AssemblyFileVersion("1.0.0.$WCREV$")]
