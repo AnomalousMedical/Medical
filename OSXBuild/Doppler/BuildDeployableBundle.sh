0e2c2d32c2b43e21cf0efb0f44fbb75b28e86f10
Andrew Piper <threax@users.noreply.github.com>
2011-02-09 14:10:13 +0000

    Added scripts to build deployable bundles for piper jbo and doppler.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1705 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- OSXBuild/Doppler/BuildDeployableBundle.sh
+++ OSXBuild/Doppler/BuildDeployableBundle.sh
@@ -6,7 +6,7 @@
 # Created by Andrew Piper on 1/18/11.
 # Copyright 2011 Anomalous Software. All rights reserved.
 
-APP_ORIGINAL_NAME="PiperJBO"
-APP_FINAL_NAME="Piper's Joint Based Occlusion"
+APP_ORIGINAL_NAME="Doppler"
+APP_FINAL_NAME="Doppler Diagnostic Module"
 
 sh ../MakeDeployableBundle.sh "$APP_ORIGINAL_NAME" "$APP_FINAL_NAME"
