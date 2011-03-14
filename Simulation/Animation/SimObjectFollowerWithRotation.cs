ade623b02e08281d13f053331dc4c1a44bdc00c3
Andrew Piper <threax@users.noreply.github.com>
2011-03-14 20:49:07 +0000

    Added a class to move an object with another object.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1808 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ Simulation/Animation/SimObjectFollowerWithRotation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine;
+using Engine.Editing;
+using Engine.ObjectManagement;
+using Engine.Platform;
+
+namespace Medical
+{
+    class SimObjectFollowerWithRotation : Behavior
+    {
+        [Editable]
+        String targetSimObjectName;
+
+        SimObject targetSimObject;
+        Vector3 translationOffset;
+
+        protected override void link()
+        {
+            targetSimObject = Owner.getOtherSimObject(targetSimObjectName);
+            if (targetSimObject == null)
+            {
+                blacklist("Cannot find target SimObject {0}.", targetSimObjectName);
+            }
+            translationOffset = Owner.Translation - targetSimObject.Translation;
+            translationOffset = Quaternion.quatRotate(targetSimObject.Rotation.inverse(), translationOffset);
+        }
+
+        public override void update(Clock clock, EventManager eventManager)
+        {
+            Vector3 trans = targetSimObject.Translation + Quaternion.quatRotate(targetSimObject.Rotation, translationOffset);
+            Quaternion rotation = targetSimObject.Rotation;
+            updatePosition(ref trans, ref rotation);
+        }
+    }
+}
