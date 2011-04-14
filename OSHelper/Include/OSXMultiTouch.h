77f1d36d1aa1754f3a7f9a41732d474ce06805c6
Andrew Piper <threax@users.noreply.github.com>
2011-04-14 18:41:52 +0000

    Added an OSX folder with a xcode project to OSHelper.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1840 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ OSHelper/Include/OSXMultiTouch.h
@@ -0,0 +1,11 @@
+//
+//  OSXMultiTouch.h
+//  OSHelper
+//
+//  Created by Andrew Piper on 1/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+class MultiTouch;
+
+void registerWithObjectiveC(void* windowHandle, MultiTouch* multiTouch);
