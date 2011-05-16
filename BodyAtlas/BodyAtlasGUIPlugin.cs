e5f1775b4b8331aabde9874c340f798585fb113e
Andrew Piper <threax@users.noreply.github.com>
2011-05-16 14:07:51 +0000

    Added BodyAtlas program. Still mostly Piper's JBO.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@1946 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ BodyAtlas/BodyAtlasGUIPlugin.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine.ObjectManagement;
+using MyGUIPlugin;
+using Medical.Controller;
+using OgreWrapper;
+using System.Diagnostics;
+
+namespace Medical.GUI
+{
+    class BodyAtlasGUIPlugin : GUIPlugin
+    {
+        private StandaloneController standaloneController;
+        private GUIManager guiManager;
+        private BodyAtlasAppMenu appMenu;
+        private CloneWindowTaskbarItem cloneWindow;
+        private RecentDocuments recentDocuments;
+        private SystemMenu systemMenu;
+        private LicenseManager licenseManager;
+        private BodyAtlasController bodyAtlasController;
+
+        //Dialogs
+        //private MandibleMovementDialog mandibleMovementDialog;
+        private NotesDialog notesDialog;
+        private StateListPopup stateList;
+        private ChooseSceneDialog chooseSceneDialog;
+        private SavePatientDialog savePatientDialog;
+        private OpenPatientDialog openPatientDialog;
+        private OptionsDialog options;
+        private RenderPropertiesDialog renderDialog;
+        private WindowLayout windowLayout;
+        private SequencePlayer sequencePlayer;
+        private AnatomyFinder anatomyFinder;
+        private BookmarksGUI bookmarks;
+        private AboutDialog aboutDialog;
+
+        public BodyAtlasGUIPlugin(LicenseManager licenseManager, BodyAtlasController bodyAtlasController)
+        {
+            this.licenseManager = licenseManager;
+            this.bodyAtlasController = bodyAtlasController;
+            recentDocuments = new RecentDocuments(MedicalConfig.RecentDocsFile);
+        }
+
+        public void Dispose()
+        {
+            recentDocuments.save();
+            sequencePlayer.Dispose();
+            windowLayout.Dispose();
+            renderDialog.Dispose();
+            options.Dispose();
+            chooseSceneDialog.Dispose();
+            savePatientDialog.Dispose();
+            openPatientDialog.Dispose();
+            appMenu.Dispose();
+            aboutDialog.Dispose();
+            //mandibleMovementDialog.Dispose();
+            notesDialog.Dispose();
+            stateList.Dispose();
+            anatomyFinder.Dispose();
+            bookmarks.Dispose();
+        }
+
+        public void initializeGUI(StandaloneController standaloneController, GUIManager guiManager)
+        {
+            this.guiManager = guiManager;
+            this.standaloneController = standaloneController;
+
+            OgreResourceGroupManager.getInstance().addResourceLocation("GUI/PiperJBO/Imagesets", "EngineArchive", "MyGUI", true);
+            Gui.Instance.load("Imagesets.xml");
+
+            appMenu = new BodyAtlasAppMenu(this, standaloneController);
+            guiManager.setAppMenu(appMenu);
+        }
+
+        public void createDialogs(DialogManager dialogManager)
+        {
+            //PiperJBO
+            //mandibleMovementDialog = new MandibleMovementDialog(standaloneController.MovementSequenceController);
+            //dialogManager.addManagedDialog(mandibleMovementDialog);
+
+            //Common
+            notesDialog = new NotesDialog(standaloneController.MedicalStateController);
+            dialogManager.addManagedDialog(notesDialog);
+
+            stateList = new StateListPopup(standaloneController.MedicalStateController);
+            dialogManager.addManagedDialog(stateList);
+
+            aboutDialog = new AboutDialog(licenseManager);
+
+            chooseSceneDialog = new ChooseSceneDialog();
+            chooseSceneDialog.ChooseScene += new EventHandler(chooseSceneDialog_ChooseScene);
+
+            savePatientDialog = new SavePatientDialog();
+            savePatientDialog.SaveFile += new EventHandler(savePatientDialog_SaveFile);
+
+            openPatientDialog = new OpenPatientDialog();
+            openPatientDialog.OpenFile += new EventHandler(openPatientDialog_OpenFile);
+
+            options = new OptionsDialog();
+            options.VideoOptionsChanged += new EventHandler(options_VideoOptionsChanged);
+
+            renderDialog = new RenderPropertiesDialog(standaloneController.SceneViewController, standaloneController.ImageRenderer);
+            dialogManager.addManagedDialog(renderDialog);
+
+            windowLayout = new WindowLayout(standaloneController);
+            dialogManager.addManagedDialog(windowLayout);
+
+            sequencePlayer = new SequencePlayer(standaloneController.MovementSequenceController);
+            dialogManager.addManagedDialog(sequencePlayer);
+
+            anatomyFinder = new AnatomyFinder(bodyAtlasController.AnatomyController, standaloneController.SceneViewController);
+            dialogManager.addManagedDialog(anatomyFinder);
+
+            bookmarks = new BookmarksGUI(bodyAtlasController.BookmarksController);
+        }
+
+        public void addToTaskbar(Taskbar taskbar)
+        {
+            taskbar.addItem(new ShowPopupTaskbarItem(bookmarks, "Bookmarks", "FavoritesIcon"));
+            taskbar.addItem(new DialogOpenTaskbarItem(anatomyFinder, "Anatomy Finder", "SearchIcon"));
+            //taskbar.addItem(new ShowToothContactsTaskbarItem());
+            taskbar.addItem(new DialogOpenTaskbarItem(stateList, "States", "Joint"));
+            taskbar.addItem(new DialogOpenTaskbarItem(notesDialog, "Notes", "Notes"));
+            taskbar.addItem(new DialogOpenTaskbarItem(sequencePlayer, "Sequences", "SequenceIconLarge"));
+            //taskbar.addItem(new DialogOpenTaskbarItem(mandibleMovementDialog, "Manual Movement", "MovementIcon"));
+            taskbar.addItem(new DialogOpenTaskbarItem(windowLayout, "Window Layout", "WindowLayoutIconLarge"));
+
+            DialogOpenTaskbarItem renderTaskbarItem = new DialogOpenTaskbarItem(renderDialog, "Render", "RenderIconLarge");
+            renderTaskbarItem.RightClicked += new EventHandler(renderTaskbarItem_RightClicked);
+            taskbar.addItem(renderTaskbarItem);
+
+            //cloneWindow = new CloneWindowTaskbarItem(standaloneController);
+            //if (PlatformConfig.AllowCloneWindows && licenseManager.allowFeature((int)Features.PIPER_JBO_FEATURE_CLONE_WINDOW))
+            //{
+            //    taskbar.addItem(cloneWindow);
+            //}
+        }
+
+        public void finishInitialization()
+        {
+            bool keyValid = licenseManager.KeyValid;
+            if (!keyValid)
+            {
+                if (licenseManager.IsExpired)
+                {
+                    MessageBox.show("Your trial has expired. Would you like to go to AnomalousMedical.com and upgrade?", "Trial Expired", MessageBoxStyle.Yes | MessageBoxStyle.No | MessageBoxStyle.IconQuest, goToWebsiteCallback);
+                }
+                else
+                {
+                    startKeyDialog();
+                }
+            }
+        }
+
+        private void goToWebsiteCallback(MessageBoxStyle result)
+        {
+            if (result == MessageBoxStyle.Yes)
+            {
+                Process.Start("http://www.anomalousmedical.com");
+            }
+            startKeyDialog();
+        }
+
+        private void startKeyDialog()
+        {
+            licenseManager.KeyEnteredSucessfully += new EventHandler(licenseManager_KeyEnteredSucessfully);
+            licenseManager.KeyInvalid += new EventHandler(licenseManager_KeyInvalid);
+            licenseManager.showKeyDialog(bodyAtlasController.ProductID);
+        }
+
+        void licenseManager_KeyInvalid(object sender, EventArgs e)
+        {
+            standaloneController.closeMainWindow();
+        }
+
+        void licenseManager_KeyEnteredSucessfully(object sender, EventArgs e)
+        {
+            MessageBox.show("Please restart to apply your license changes.", "Restart required", MessageBoxStyle.Ok | MessageBoxStyle.IconInfo, restartMessageClosed);
+        }
+
+        void restartMessageClosed(MessageBoxStyle result)
+        {
+            standaloneController.closeMainWindow();
+        }
+
+        public void sceneLoaded(SimScene scene)
+        {
+            //mandibleMovementDialog.sceneLoaded(scene);
+        }
+
+        public void sceneUnloading(SimScene scene)
+        {
+            //mandibleMovementDialog.sceneUnloading(scene);
+        }
+
+        public void setMainInterfaceEnabled(bool enabled)
+        {
+            if (systemMenu != null)
+            {
+                systemMenu.FileMenuEnabled = enabled;
+            }
+        }
+
+        public void createMenuBar(NativeMenuBar menu)
+        {
+            systemMenu = new SystemMenu(menu, this, standaloneController, licenseManager);
+        }
+
+        public void showOptions()
+        {
+            options.Visible = true;
+        }
+
+        public void showAboutDialog()
+        {
+            aboutDialog.open(true);
+        }
+
+        internal void toggleCloneWindow()
+        {
+            cloneWindow.toggleCloneWindow();
+        }
+
+        public void showChooseSceneDialog()
+        {
+            chooseSceneDialog.open(true);
+        }
+
+        public void open()
+        {
+            openPatientDialog.open(true);
+        }
+
+        public void save()
+        {
+            if (standaloneController.MedicalStateController.getNumStates() == 0)
+            {
+                MessageBox.show("No information to save. Please create some states using the wizards first.", "Nothing to save.", MessageBoxStyle.IconInfo | MessageBoxStyle.Ok);
+            }
+            else
+            {
+                savePatientDialog.save();
+            }
+        }
+
+        public void saveAs()
+        {
+            if (standaloneController.MedicalStateController.getNumStates() == 0)
+            {
+                MessageBox.show("No information to save. Please create some states using the wizards first.", "Nothing to save.", MessageBoxStyle.IconInfo | MessageBoxStyle.Ok);
+            }
+            else
+            {
+                savePatientDialog.saveAs();
+            }
+        }
+
+        public void changeActiveFile(PatientDataFile patientData)
+        {
+            if (patientData != null)
+            {
+                MainWindow.Instance.updateWindowTitle(String.Format("{0} {1}", patientData.FirstName, patientData.LastName));
+                recentDocuments.addDocument(patientData.BackingFile);
+            }
+            else
+            {
+                MainWindow.Instance.clearWindowTitle();
+            }
+        }
+
+        public RecentDocuments RecentDocuments
+        {
+            get
+            {
+                return recentDocuments;
+            }
+        }
+
+        private void savePatientDialog_SaveFile(object sender, EventArgs e)
+        {
+            PatientDataFile patientData = savePatientDialog.PatientData;
+            changeActiveFile(patientData);
+            standaloneController.saveMedicalState(patientData);
+        }
+
+        private void openPatientDialog_OpenFile(object sender, EventArgs e)
+        {
+            PatientDataFile patientData = openPatientDialog.CurrentFile;
+            changeActiveFile(patientData);
+            standaloneController.openPatientFile(patientData);
+        }
+
+        private void options_VideoOptionsChanged(object sender, EventArgs e)
+        {
+            standaloneController.recreateMainWindow();
+        }
+
+        private void chooseSceneDialog_ChooseScene(object sender, EventArgs e)
+        {
+            changeActiveFile(null);
+            standaloneController.openNewScene(chooseSceneDialog.SelectedFile);
+        }
+
+        void renderTaskbarItem_OnClicked(object sender, EventArgs e)
+        {
+            renderDialog.render();
+        }
+
+        void renderTaskbarItem_RightClicked(object sender, EventArgs e)
+        {
+            renderDialog.render();
+        }
+    }
+}
---
+++ BodyAtlas/GUI/Menus/SystemMenu.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Medical.Controller;
+
+namespace Medical.GUI
+{
+    class SystemMenu
+    {
+        private RecentDocuments recentDocuments;
+
+        private NativeMenuItem recentPatients;
+        private Dictionary<String, NativeMenuItem> recentDocMenuItems = new Dictionary<string, NativeMenuItem>();
+        private Dictionary<int, string> menuIDsToFiles = new Dictionary<int, string>();
+
+        private NativeMenu fileMenu;
+        private NativeMenuItem changeScene;
+        private NativeMenuItem open;
+        private NativeMenuItem save;
+        private NativeMenuItem saveAs;
+        private NativeMenuItem exit;
+
+        private BodyAtlasGUIPlugin piperGUI;
+        private StandaloneController standaloneController;
+
+        public SystemMenu(NativeMenuBar menu, BodyAtlasGUIPlugin piperGUI, StandaloneController standaloneController, LicenseManager licenseManager)
+        {
+            this.piperGUI = piperGUI;
+            this.standaloneController = standaloneController;
+            this.recentDocuments = piperGUI.RecentDocuments;
+
+            //File menu
+            fileMenu = menu.createMenu("&File");
+
+            changeScene = fileMenu.append(CommonMenuItems.New, "&New Scene...\tCtrl+N", "Change to a new scene.");
+            changeScene.Select += new NativeMenuEvent(changeScene_Select);
+
+            open = fileMenu.append(CommonMenuItems.Open, "&Open...\tCtrl+O", "Open existing distortions.");
+            open.Select += new NativeMenuEvent(open_Select);
+
+            recentPatients = fileMenu.append(CommonMenuItems.AutoAssign, "Recent Patients", "", true);
+            foreach (String document in recentDocuments)
+            {
+                createWindowMenuDocument(document);
+            }
+            recentDocuments.DocumentAdded += new RecentDocumentEvent(recentDocuments_DocumentAdded);
+            recentDocuments.DocumentReaccessed += new RecentDocumentEvent(recentDocuments_DocumentReaccessed);
+            recentDocuments.DocumentRemoved += new RecentDocumentEvent(recentDocuments_DocumentRemoved);
+
+            save = fileMenu.append(CommonMenuItems.Save, "&Save...\tCtrl+S", "Save current distortions.");
+            save.Select += new NativeMenuEvent(save_Select);
+
+            saveAs = fileMenu.append(CommonMenuItems.SaveAs, "Save &As...", "Save current distortions as.");
+            saveAs.Select += new NativeMenuEvent(saveAs_Select);
+
+            fileMenu.appendSeparator();
+
+            NativeMenuItem preferences = fileMenu.append(CommonMenuItems.Preferences, "Preferences", "Set program configuration.");
+            preferences.Select += new NativeMenuEvent(preferences_Select);
+
+            exit = fileMenu.append(CommonMenuItems.Exit, "&Exit", "Exit the program.");
+            exit.Select += new NativeMenuEvent(exit_Select);
+
+            menu.append(fileMenu);
+
+            //if (PlatformConfig.AllowCloneWindows && licenseManager.allowFeature((int)Features.PIPER_JBO_FEATURE_CLONE_WINDOW))
+            //{
+            //    //Utilities Menu
+            //    NativeMenu utilitiesMenu = menu.createMenu("&Utilities");
+
+            //    NativeMenuItem cloneWindow = utilitiesMenu.append(CommonMenuItems.AutoAssign, "Clone Window", "Open a window that displays the main window with no controls.");
+            //    cloneWindow.Select += new NativeMenuEvent(cloneWindow_Select);
+
+            //    menu.append(utilitiesMenu);
+            //}
+
+            //Help Menu
+            NativeMenu helpMenu = menu.createMenu("&Help");
+
+            NativeMenuItem help = helpMenu.append(CommonMenuItems.Help, "Piper's JBO Help", "Open Piper's JBO user manual.");
+            help.Select += new NativeMenuEvent(help_Select);
+
+            NativeMenuItem about = helpMenu.append(CommonMenuItems.About, "About", "About this program.");
+            about.Select += new NativeMenuEvent(about_Select);
+
+            menu.append(helpMenu);
+        }
+
+        void cloneWindow_Select(NativeMenuItem sender)
+        {
+            piperGUI.toggleCloneWindow();
+        }
+
+        void preferences_Select(NativeMenuItem sender)
+        {
+            piperGUI.showOptions();
+        }
+
+        void help_Select(NativeMenuItem sender)
+        {
+            standaloneController.openHelpTopic(0);
+        }
+
+        void about_Select(NativeMenuItem sender)
+        {
+            piperGUI.showAboutDialog();
+        }
+
+        public bool FileMenuEnabled
+        {
+            get
+            {
+                return changeScene.Enabled;
+            }
+            set
+            {
+                changeScene.Enabled = value;
+                open.Enabled = value;
+                save.Enabled = value;
+                saveAs.Enabled = value;
+                recentPatients.Enabled = value;
+            }
+        }
+
+        void exit_Select(NativeMenuItem sender)
+        {
+            standaloneController.closeMainWindow();
+        }
+
+        void saveAs_Select(NativeMenuItem sender)
+        {
+            piperGUI.saveAs();
+        }
+
+        void save_Select(NativeMenuItem sender)
+        {
+            piperGUI.save();
+        }
+
+        void open_Select(NativeMenuItem sender)
+        {
+            piperGUI.open();
+        }
+
+        void changeScene_Select(NativeMenuItem sender)
+        {
+            piperGUI.showChooseSceneDialog();
+        }
+
+        void recentDocMenuItem_Click(NativeMenuItem sender)
+        {
+            String document = menuIDsToFiles[sender.ID];
+            PatientDataFile patientData = new PatientDataFile(document);
+            if (patientData.loadHeader())
+            {
+                piperGUI.changeActiveFile(patientData);
+                standaloneController.openPatientFile(patientData);
+            }
+            else
+            {
+                MyGUIPlugin.MessageBox.show(String.Format("Error loading file {0}.", patientData.BackingFile), "Load Error", MyGUIPlugin.MessageBoxStyle.Ok | MyGUIPlugin.MessageBoxStyle.IconError);
+            }
+        }
+
+        void recentDocuments_DocumentRemoved(RecentDocuments source, string document)
+        {
+            NativeMenuItem recentDocMenuItem;
+            if (recentDocMenuItems.TryGetValue(document, out recentDocMenuItem))
+            {
+                recentDocMenuItems.Remove(document);
+                menuIDsToFiles.Remove(recentDocMenuItem.ID);
+                recentPatients.SubMenu.remove(recentDocMenuItem);
+                recentDocMenuItem.Dispose();
+            }
+        }
+
+        void recentDocuments_DocumentReaccessed(RecentDocuments source, string document)
+        {
+            NativeMenuItem recentDocMenuItem;
+            if (recentDocMenuItems.TryGetValue(document, out recentDocMenuItem))
+            {
+                recentPatients.SubMenu.remove(recentDocMenuItem);
+                recentPatients.SubMenu.insert(0, recentDocMenuItem);
+            }
+        }
+
+        void recentDocuments_DocumentAdded(RecentDocuments source, string document)
+        {
+            createWindowMenuDocument(document);
+        }
+
+        void createWindowMenuDocument(String document)
+        {
+            if (recentPatients != null)
+            {
+                NativeMenuItem recentDocMenuItem = recentPatients.SubMenu.insert(0, CommonMenuItems.AutoAssign, Path.GetFileNameWithoutExtension(document), "");
+                recentDocMenuItem.Select += new NativeMenuEvent(recentDocMenuItem_Click);
+                recentDocMenuItem.Help = document;
+                menuIDsToFiles.Add(recentDocMenuItem.ID, document);
+                recentDocMenuItems.Add(document, recentDocMenuItem);
+            }
+        }
+    }
+}
