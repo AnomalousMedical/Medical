a81fdd7e6840640fb177e83597afa914b8d5421a
Andrew Piper <threax@users.noreply.github.com>
2011-07-15 17:15:49 +0000

    Added a TaskMenu/StartEmbeddedTimelineTaskMenuItem.cs.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@2188 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ Standalone/GUI/TaskMenu/StartEmbeddedTimelineTaskMenuItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Medical.GUI
+{
+    /// <summary>
+    /// This class can be used by plugins to start their main timelines.
+    /// </summary>
+    public class StartEmbeddedTimelineTaskMenuItem : TaskMenuItem
+    {
+        private String startTimeline;
+        private TimelineController timelineController;
+        private Assembly assembly;
+        private String resourceRoot;
+
+        public StartEmbeddedTimelineTaskMenuItem(String name, String iconName, String category, Type typeInAssembly, String resourceRoot, String startTimeline, TimelineController timelineController)
+            : this(name, iconName, category, typeInAssembly, resourceRoot, startTimeline, timelineController, DEFAULT_WEIGHT)
+        {
+
+        }
+
+        public StartEmbeddedTimelineTaskMenuItem(String name, String iconName, String category, Type typeInAssembly, String resourceRoot, String startTimeline, TimelineController timelineController, int weight)
+            :base(name, iconName, category)
+        {
+            this.ShowOnTaskbar = false;
+            this.Weight = weight;
+            this.startTimeline = startTimeline;
+            this.timelineController = timelineController;
+            this.assembly = typeInAssembly.Assembly;
+            this.resourceRoot = resourceRoot;
+        }
+
+        public override void clicked()
+        {
+            timelineController.ResourceProvider = new TimelineEmbeddedResourceProvider(assembly, resourceRoot); ;
+            Timeline start = timelineController.openTimeline(startTimeline);
+            timelineController.startPlayback(start);
+        }
+    }
+}
