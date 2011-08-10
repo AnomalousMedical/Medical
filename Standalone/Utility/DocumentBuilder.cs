8506b6cd150e179ac41f0ebcd2bf737dded2d9ed
Andrew Piper <threax@users.noreply.github.com>
2011-08-10 19:09:57 +0000

    Added a DocumentBuilder class to assist in making documents.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@2277 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ Standalone/Utility/DocumentBuilder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Medical
+{
+    public class DocumentBuilder
+    {
+        private StringBuilder stringBuilder;
+
+        public DocumentBuilder()
+        {
+            stringBuilder = new StringBuilder();
+        }
+
+        public DocumentBuilder(int capacity)
+        {
+            stringBuilder = new StringBuilder(capacity);
+        }
+
+        public void writeLine(String text)
+        {
+            stringBuilder.AppendLine(text);
+        }
+
+        public void writeLine(String text, params Object[] args)
+        {
+            stringBuilder.AppendFormat(text, args);
+            stringBuilder.AppendLine();
+        }
+
+        public void breakLine()
+        {
+            stringBuilder.AppendLine();
+        }
+
+        public void writeSentence(String text)
+        {
+            stringBuilder.Append(text);
+            stringBuilder.Append("  ");
+        }
+
+        public void writeSentence(String text, params Object[] args)
+        {
+            stringBuilder.AppendFormat(text, args);
+            stringBuilder.Append("  ");
+        }
+
+        public void startParagraph()
+        {
+            stringBuilder.Append('\t');
+        }
+
+        public void endParagraph()
+        {
+            stringBuilder.Append("\n\n");
+        }
+
+        public override string ToString()
+        {
+            return stringBuilder.ToString();
+        }
+    }
+}
