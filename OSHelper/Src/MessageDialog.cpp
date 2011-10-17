2502779c4437ad52485faddc86c76635ef12e3a1
Andrew Piper <threax@users.noreply.github.com>
2011-10-17 17:18:04 +0000

    Made message boxes work on osx correctly.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@2531 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- OSHelper/Src/MessageDialog.cpp
+++ OSHelper/Src/MessageDialog.cpp
@@ -2,6 +2,8 @@
 #include "Enums.h"
 #include "NativeOSWindow.h"
 
+#ifdef WINDOWS
+
 extern "C" _AnomalousExport void MessageDialog_showErrorDialog(NativeOSWindow* parent, String msg, String cap)
 {
 	wxMessageBox(wxString::FromAscii(msg), wxString::FromAscii(cap), wxOK | wxICON_ERROR, parent);
@@ -10,5 +12,48 @@ extern "C" _AnomalousExport void MessageDialog_showErrorDialog(NativeOSWindow* p
 extern "C" _AnomalousExport NativeDialogResult MessageDialog_showQuestionDialog(NativeOSWindow* parent, String msg, String cap)
 {
 	return interpretResults(wxMessageBox(wxString::FromAscii(msg), wxString::FromAscii(cap), wxYES_NO | wxICON_QUESTION, parent));
-	
 }
+
+#endif
+
+#ifdef MAC_OSX
+
+#include <CoreFoundation/CoreFoundation.h>
+
+extern "C" _AnomalousExport void MessageDialog_showErrorDialog(NativeOSWindow* parent, String msg, String cap)
+{
+	CFStringRef cfCap = CFStringCreateWithCString(NULL, cap, kCFStringEncodingUTF8);
+	CFStringRef cfMsg = CFStringCreateWithCString(NULL, msg, kCFStringEncodingUTF8);
+	
+	CFOptionFlags response;
+	CFUserNotificationDisplayAlert(0, kCFUserNotificationCautionAlertLevel, NULL, NULL, NULL, cfCap, cfMsg, CFSTR("Ok"), NULL, NULL, &response);
+	
+	CFRelease(cfCap);
+	CFRelease(cfMsg);
+}
+
+extern "C" _AnomalousExport NativeDialogResult MessageDialog_showQuestionDialog(NativeOSWindow* parent, String msg, String cap)
+{
+	CFStringRef cfCap = CFStringCreateWithCString(NULL, cap, kCFStringEncodingUTF8);
+	CFStringRef cfMsg = CFStringCreateWithCString(NULL, msg, kCFStringEncodingUTF8);
+	
+	CFOptionFlags response;
+	CFUserNotificationDisplayAlert(0, kCFUserNotificationCautionAlertLevel, NULL, NULL, NULL, cfCap, cfMsg, CFSTR("Yes"), CFSTR("No"), NULL, &response);
+	
+	CFRelease(cfCap);
+	CFRelease(cfMsg);
+	
+	switch (response) {
+		case kCFUserNotificationDefaultResponse:
+			return YES;
+			break;
+		case kCFUserNotificationAlternateResponse:
+			return NO;
+			break;
+		default:
+			return CANCEL;
+			break;
+	}
+}
+
+#endif
