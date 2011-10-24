058cc2f886ae0b6bf35d1d9658b1d4ba4146d4f8
Andrew Piper <threax@users.noreply.github.com>
2011-10-24 20:13:13 +0000

    Added a DentalSim dll project for the dental simulation plugin.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@2575 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ DentalSim/DentalSimAtlasPluginEntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Medical;
+
+namespace DentalSim
+{
+    class DentalSimAtlasPluginEntryPoint : AtlasPluginEntryPointAttribute
+    {
+        public override void createPlugin(StandaloneController standaloneController)
+        {
+            if (standaloneController.App.LicenseManager.allowFeature(2))
+            {
+                standaloneController.AtlasPluginManager.addPlugin(new DentalSimPlugin());
+            }
+        }
+    }
+}
---
+++ DentalSim/DentalSimPlugin.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Medical;
+using Engine.ObjectManagement;
+using Medical.GUI;
+using MyGUIPlugin;
+
+namespace DentalSim
+{
+    class DentalSimPlugin : AtlasPlugin
+    {
+        public DentalSimPlugin()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public void createMenuBar(NativeMenuBar menu)
+        {
+
+        }
+
+        public void initialize(StandaloneController standaloneController)
+        {
+            GUIManager guiManager = standaloneController.GUIManager;
+            Gui.Instance.load("Medical.Resources.EditorImagesets.xml");
+        }
+
+        public void sceneLoaded(SimScene scene)
+        {
+
+        }
+
+        public void sceneRevealed()
+        {
+
+        }
+
+        public void sceneUnloading(Engine.ObjectManagement.SimScene scene)
+        {
+
+        }
+
+        public void setMainInterfaceEnabled(bool enabled)
+        {
+
+        }
+
+        public string BrandingImageKey
+        {
+            get
+            {
+                return "DentalSim/BrandingImage";
+            }
+        }
+
+        public long PluginId
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public string PluginName
+        {
+            get
+            {
+                return "Dental Simulation";
+            }
+        }
+
+        public String Location
+        {
+            get
+            {
+                return GetType().Assembly.Location;
+            }
+        }
+
+        public Version Version
+        {
+            get
+            {
+                return GetType().Assembly.GetName().Version;
+            }
+        }
+    }
+}
