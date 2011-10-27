1331dbc466d87bfad1306d3799ad04bd2c4bb2b9
Andrew Piper <threax@users.noreply.github.com>
2011-10-27 18:00:35 +0000

    Added a GraphicsUser project that will have the renderer for current graphics users in it.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@2599 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ GraphicsUser/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GraphicsUser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Anomalous Medical")]
+[assembly: AssemblyProduct("Anomalous Medical")]
+[assembly: AssemblyCopyright("Copyright © Anomalous Medical 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("37e6e081-d0d9-46cb-92d6-9440a0e3b338")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
