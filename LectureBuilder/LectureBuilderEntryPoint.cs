ff84ec942a40d582fc6a73e1bd15c17e220764b7
Andrew Piper <threax@users.noreply.github.com>
2012-01-16 18:22:53 +0000

    Added the skeleton of a LectureBuilder plugin.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@2773 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ LectureBuilder/LectureBuilderEntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Medical;
+
+[assembly: LectureBuilder.LectureBuilderEntryPoint()]
+
+namespace LectureBuilder
+{
+    class LectureBuilderEntryPoint : AtlasPluginEntryPointAttribute
+    {
+        public override void createPlugin(StandaloneController standaloneController)
+        {
+            standaloneController.AtlasPluginManager.addPlugin(new LectureBuilderPlugin());
+        }
+    }
+}
---
+++ LectureBuilder/LectureBuilderPlugin.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Medical;
+using Engine.ObjectManagement;
+
+namespace LectureBuilder
+{
+    class LectureBuilderPlugin : AtlasPlugin
+    {
+        public LectureBuilderPlugin()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void loadGUIResources()
+        {
+            
+        }
+
+        public void initialize(StandaloneController standaloneController)
+        {
+            
+        }
+
+        public void sceneLoaded(SimScene scene)
+        {
+            
+        }
+
+        public void sceneUnloading(SimScene scene)
+        {
+            
+        }
+
+        public void setMainInterfaceEnabled(bool enabled)
+        {
+            
+        }
+
+        public void createMenuBar(NativeMenuBar menu)
+        {
+            
+        }
+
+        public void sceneRevealed()
+        {
+            
+        }
+
+        public long PluginId
+        {
+            get
+            {
+                return 10;
+            }
+        }
+
+        public string PluginName
+        {
+            get
+            {
+                return "Lecture Builder";
+            }
+        }
+
+        public string BrandingImageKey
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public String Location
+        {
+            get
+            {
+                return GetType().Assembly.Location;
+            }
+        }
+
+        public Version Version
+        {
+            get
+            {
+                return GetType().Assembly.GetName().Version;
+            }
+        }
+    }
+}
