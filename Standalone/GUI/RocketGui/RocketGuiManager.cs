70cdfa00962856885b291f3e6637ab36af485551
Andrew Piper <threax@users.noreply.github.com>
2012-04-26 20:33:27 +0000

    Started moving the rocket / mygui stuff into its own class.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@2915 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Standalone/GUI/RocketGui/RocketGuiManager.cs
+++ Standalone/GUI/RocketGui/RocketGuiManager.cs
@@ -12,17 +12,8 @@ namespace Medical.GUI
 {
     class RocketGuiManager : IDisposable
     {
-        private SceneManager sceneManager;
-        //private OgreWindow ogreWindow;
-        private Camera camera;
-        private Viewport vp;
-        private TexturePtr texture;
-        HardwarePixelBufferSharedPtr pixelBuffer;
-
-        private Context context;
-        private ContextUpdater contextUpdater;
         private EventListenerInstancer eventListenerInstancer;
-        private ContextWindowListener windowListener;
+        private RocketWidget rocketWidget;
 
         public RocketGuiManager()
         {
@@ -31,35 +22,13 @@ namespace Medical.GUI
 
         public void Dispose()
         {
-            if (windowListener != null)
+            if (rocketWidget != null)
             {
-                MainWindow.Instance.removeListener(windowListener);
-            }
-            if (contextUpdater != null)
-            {
-                contextUpdater.Dispose();
-            }
-            if (context != null)
-            {
-                context.Dispose();
+                rocketWidget.Dispose();
             }
             if (eventListenerInstancer != null)
             {
                 eventListenerInstancer.Dispose();
-            }
-
-            if (vp != null)
-            {
-                pixelBuffer.Value.getRenderTarget().destroyViewport(vp);
-                //ogreWindow.OgreRenderWindow.destroyViewport(vp);
-            }
-            if (camera != null)
-            {
-                sceneManager.destroyCamera(camera);
-            }
-            if (sceneManager != null)
-            {
-                Root.getSingleton().destroySceneManager(sceneManager);
             }
         }
 
@@ -67,22 +36,6 @@ namespace Medical.GUI
         {
             //Create a rocket group in ogre
             OgreResourceGroupManager.getInstance().createResourceGroup("Rocket");
-
-            sceneManager = Root.getSingleton().createSceneManager(SceneType.ST_GENERIC, "libRocketScene");
-            //ogreWindow = pluginManager.RendererPlugin.PrimaryWindow as OgreWindow;
-
-            //Create camera and viewport
-            camera = sceneManager.createCamera("libRocketCamera");
-
-            texture = TextureManager.getInstance().createManual("__RocketRTT", "Rocket", TextureType.TEX_TYPE_2D, (uint)2048, (uint)2048, 1, 1, OgreWrapper.PixelFormat.PF_A8R8G8B8, TextureUsage.TU_RENDERTARGET, false, 0);
-
-            //vp = ogreWindow.OgreRenderWindow.addViewport(camera, 2000000, 0.0f, 0.0f, 1.0f, 1.0f);
-            pixelBuffer = texture.Value.getBuffer();
-            vp = pixelBuffer.Value.getRenderTarget().addViewport(camera);
-            vp.setBackgroundColor(new Color(0.0f, 0.0f, 0.0f, 0.0f));
-            vp.setOverlaysEnabled(false);
-            //vp.setClearEveryFrame(false);
-            vp.clear();
 
             eventListenerInstancer = new TestEventListenerInstancer();
             Factory.RegisterEventListenerInstancer(eventListenerInstancer);
@@ -96,34 +49,8 @@ namespace Medical.GUI
             FontDatabase.LoadFontFace("assets/Delicious-Italic.otf");
             FontDatabase.LoadFontFace("assets/Delicious-BoldItalic.otf");
 
-            //context = Core.CreateContext("main", new Vector2i((int)ogreWindow.OgreRenderWindow.getWidth(), (int)ogreWindow.OgreRenderWindow.getHeight()));
-            context = Core.CreateContext("main", new Vector2i(2048, 2048));
-            Debugger.Initialise(context);
-
-            //windowListener = new ContextWindowListener(context);
-            //MainWindow.Instance.addListener(windowListener);
-
-            //using (ElementDocument cursor = context.LoadMouseCursor(sample_path + "assets/cursor.rml"))
-            //{
-
-            //}
-
-            using (ElementDocument document = context.LoadDocument("assets/demo.rml"))
-            {
-                if (document != null)
-                {
-                    document.Show();
-                }
-            }
-
-            sceneManager.addRenderQueueListener(new RocketRenderQueueListener(context, (RenderInterfaceOgre3D)Core.GetRenderInterface()));
-            contextUpdater = new ContextUpdater(context, eventManager);
-            mainTimer.addFixedUpdateListener(contextUpdater);
-
-            MyGUIPlugin.ImageBox imagebox = (MyGUIPlugin.ImageBox)MyGUIPlugin.Gui.Instance.createWidgetT("ImageBox", "ImageBox", 0, 0, 2048, 2048, MyGUIPlugin.Align.Default, "Overlapped", "RocketTestImage");
-            imagebox.setImageTexture("__RocketRTT");
-            //imagebox.setImageCoord(new MyGUIPlugin.IntCoord(600, 600, 2048, 2048));
-            //imagebox.setima
+            //Debugger.Initialise(context);
+            rocketWidget = new RocketWidget("Test", eventManager, mainTimer);
         }
     }
 }
---
+++ Standalone/GUI/RocketGui/RocketWidget.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyGUIPlugin;
+using OgreWrapper;
+using libRocketPlugin;
+using Engine;
+using Engine.Platform;
+
+namespace Medical.GUI
+{
+    class RocketWidget : IDisposable
+    {
+        private SceneManager sceneManager;
+        private Camera camera;
+        private Viewport vp;
+        private TexturePtr texture;
+        private HardwarePixelBufferSharedPtr pixelBuffer;
+
+        private Context context;
+
+        private ImageBox imageBox;
+
+        //temp
+        private ContextUpdater contextUpdater;
+        //end temp
+
+        public RocketWidget(String name, EventManager eventManager, UpdateTimer mainTimer)
+        {
+            sceneManager = Root.getSingleton().createSceneManager(SceneType.ST_GENERIC, "libRocketScene");
+            //ogreWindow = pluginManager.RendererPlugin.PrimaryWindow as OgreWindow;
+
+            //Create camera and viewport
+            camera = sceneManager.createCamera("libRocketCamera");
+
+            texture = TextureManager.getInstance().createManual("__RocketRTT", "Rocket", TextureType.TEX_TYPE_2D, (uint)2048, (uint)2048, 1, 1, OgreWrapper.PixelFormat.PF_A8R8G8B8, TextureUsage.TU_RENDERTARGET, false, 0);
+
+            //vp = ogreWindow.OgreRenderWindow.addViewport(camera, 2000000, 0.0f, 0.0f, 1.0f, 1.0f);
+            pixelBuffer = texture.Value.getBuffer();
+            vp = pixelBuffer.Value.getRenderTarget().addViewport(camera);
+            vp.setBackgroundColor(new Color(0.0f, 0.0f, 0.0f, 0.0f));
+            vp.setOverlaysEnabled(false);
+            //vp.setClearEveryFrame(false);
+            vp.clear();
+
+            context = Core.CreateContext(name, new Vector2i(2048, 2048));
+
+            using (ElementDocument document = context.LoadDocument("assets/demo.rml"))
+            {
+                if (document != null)
+                {
+                    document.Show();
+                }
+            }
+
+            sceneManager.addRenderQueueListener(new RocketRenderQueueListener(context, (RenderInterfaceOgre3D)Core.GetRenderInterface()));
+            contextUpdater = new ContextUpdater(context, eventManager);
+            mainTimer.addFixedUpdateListener(contextUpdater);
+
+            imageBox = (ImageBox)Gui.Instance.createWidgetT("ImageBox", "ImageBox", 0, 0, 2048, 2048, MyGUIPlugin.Align.Default, "Overlapped", name);
+            imageBox.setImageTexture("__RocketRTT");
+            //imagebox.setImageCoord(new MyGUIPlugin.IntCoord(600, 600, 2048, 2048));
+            //imagebox.setima
+        }
+
+        public void Dispose()
+        {
+            if (imageBox != null)
+            {
+                Gui.Instance.destroyWidget(imageBox);
+            }
+            if (contextUpdater != null)
+            {
+                contextUpdater.Dispose();
+            }
+            if (context != null)
+            {
+                context.Dispose();
+            }
+            if (vp != null)
+            {
+                pixelBuffer.Value.getRenderTarget().destroyViewport(vp);
+            }
+            if (pixelBuffer != null)
+            {
+                pixelBuffer.Dispose();
+            }
+            if (texture != null)
+            {
+                texture.Dispose();
+            }
+            if (camera != null)
+            {
+                sceneManager.destroyCamera(camera);
+            }
+            if (sceneManager != null)
+            {
+                Root.getSingleton().destroySceneManager(sceneManager);
+            }
+        }
+
+    }
+}
