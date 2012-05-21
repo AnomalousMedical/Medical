0e0cec504c92edf588831aad196763c9dc5aa462
Andrew Piper <threax@users.noreply.github.com>
2012-05-21 21:02:51 +0000

    Made it so you can make new rml files.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@3101 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Editor/Controller/TypeControllers/RmlTypeController.cs
+++ Editor/Controller/TypeControllers/RmlTypeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Medical.GUI;
+using System.IO;
+using MyGUIPlugin;
 
 namespace Medical
 {
@@ -28,5 +30,56 @@ namespace Medical
             editor.activateExtensionActions();
             editor.bringToFront();
         }
+
+        public override void addCreationMethod(ContextMenu contextMenu, string path, bool isDirectory, bool isTopLevel)
+        {
+            contextMenu.add(new ContextMenuItem("Create Rml File", path, delegate(ContextMenuItem item)
+            {
+                InputBox.GetInput("Rml File Name", "Enter a name for the rml file.", true, delegate(String result, ref String errorMessage)
+                {
+                    String filePath = Path.Combine(path, result);
+                    filePath = Path.ChangeExtension(filePath, ".rml");
+                    if (editorController.ResourceProvider.exists(filePath))
+                    {
+                        MessageBox.show(String.Format("Are you sure you want to override {0}?", filePath), "Override", MessageBoxStyle.IconQuest | MessageBoxStyle.Yes | MessageBoxStyle.No, delegate(MessageBoxStyle overrideResult)
+                        {
+                            if (overrideResult == MessageBoxStyle.Yes)
+                            {
+                                createNewRmlFile(filePath);
+                            }
+                        });
+                    }
+                    else
+                    {
+                        createNewRmlFile(filePath);
+                    }
+                    return true;
+                });
+            }));
+        }
+
+        void createNewRmlFile(String filePath)
+        {
+            Timeline timeline = new Timeline();
+            using (StreamWriter sw = new StreamWriter(editorController.ResourceProvider.openWriteStream(filePath)))
+            {
+                sw.Write(defaultRml);
+            }
+            openFile(filePath);
+        }
+
+        private const String defaultRml = @"<rml>
+  <head>
+    <link type=""text/rcss"" href=""/libRocketPlugin.Resources.rkt.rcss""/>
+    <link type=""text/rcss"" href=""/libRocketPlugin.Resources.Anomalous.rcss""/>
+  </head>
+  <body>
+    <div class=""ScrollArea"">
+      <h1>Empty Rml View</h1>
+      <p>You can start creating your Rml View here. You can erase this text to start.</p>
+    </div>
+  </body>
+</rml>
+";
     }
 }
