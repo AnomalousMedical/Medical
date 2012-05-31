9a85206e34e3bea0c0e5daa7b25c85cddd91a5e9
Andrew Piper <threax@users.noreply.github.com>
2012-05-31 20:41:15 +0000

    Expanded the unit test to make lots of images.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@3138 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- UnitTestPlugin/GUI/TestImageAtlas/TestImageAtlas.cs
+++ UnitTestPlugin/GUI/TestImageAtlas/TestImageAtlas.cs
@@ -33,11 +33,28 @@ namespace UnitTestPlugin.GUI
             }
             using (Bitmap bitmap = (Bitmap)Bitmap.FromStream(Assembly.GetCallingAssembly().GetManifestResourceStream("UnitTestPlugin.Resources.DownloadIcon.png")))
             {
-                Log.Debug("Adding DownloadIcon to image atlas");
-                ImageAtlasPage page = imageAtlas.addImage("DownloadIcon", bitmap);
-                ImageBox imageBox1 = (ImageBox)window.findWidget("ImageBox2");
+                for (int i = 0; i < 10; ++i)
+                {
+                    String imageName = "DownloadIcon" + i;
+                    Log.Debug("Adding DownloadIcon to image atlas");
+                    ImageAtlasPage page = imageAtlas.addImage(imageName, bitmap);
+                    Log.Debug("Page is {0}", page.TextureName);
+                    ImageBox imageBox1 = (ImageBox)window.findWidget("ImageBox2");
+                    Rectangle coord;
+                    if (page.tryGetImageLocation(imageName, out coord))
+                    {
+                        imageBox1.setImageTexture(page.TextureName);
+                        imageBox1.setImageCoord(new IntCoord(coord.Left, coord.Top, coord.Width, coord.Height));
+                    }
+                }
+            }
+            using (Bitmap bitmap = (Bitmap)Bitmap.FromStream(Assembly.GetCallingAssembly().GetManifestResourceStream("UnitTestPlugin.Resources.LegacyLogoSmall.jpg")))
+            {
+                Log.Debug("Adding LegacyLogoSmall1 to image atlas");
+                ImageAtlasPage page = imageAtlas.addImage("LegacyLogoSmall1", bitmap);
+                ImageBox imageBox1 = (ImageBox)window.findWidget("ImageBox1");
                 Rectangle coord;
-                if (page.tryGetImageLocation("DownloadIcon", out coord))
+                if (page.tryGetImageLocation("LegacyLogoSmall1", out coord))
                 {
                     imageBox1.setImageTexture(page.TextureName);
                     imageBox1.setImageCoord(new IntCoord(coord.Left, coord.Top, coord.Width, coord.Height));
