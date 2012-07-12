da7e02a5f475d18749af39a77ea1701ac0b65d13
Andrew Piper <threax@users.noreply.github.com>
2012-07-12 14:00:27 +0000

    Finished skeleton for the presentation editor.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@3384 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ PresentationEditor/PresentationEditorAtlasPluginEntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Medical;
+
+[assembly: PresentationEditor.PresentationEditorAtlasPluginEntryPoint()]
+
+namespace PresentationEditor
+{
+    class PresentationEditorAtlasPluginEntryPoint : AtlasPluginEntryPointAttribute
+    {
+        public override void createPlugin(StandaloneController standaloneController)
+        {
+            standaloneController.AtlasPluginManager.addPlugin(new PresentationEditorPlugin());
+        }
+    }
+}
---
+++ PresentationEditor/PresentationEditorPlugin.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Medical.GUI;
+using Logging;
+using Engine.ObjectManagement;
+using MyGUIPlugin;
+using Medical.Controller;
+using Medical.Editor;
+using Medical;
+
+namespace PresentationEditor
+{
+    public class PresentationEditorPlugin : AtlasPlugin
+    {
+        private StandaloneController standaloneController;
+
+        public PresentationEditorPlugin()
+        {
+            Log.Info("Presentation Editor GUI Loaded");
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void loadGUIResources()
+        {
+            ResourceManager.Instance.load("PresentationEditor.Resources.PresentationEditorImagesets.xml");
+        }
+
+        public void initialize(StandaloneController standaloneController)
+        {
+            this.standaloneController = standaloneController;
+        }
+
+        public void sceneLoaded(SimScene scene)
+        {
+            
+        }
+
+        public void sceneUnloading(SimScene scene)
+        {
+            
+        }
+
+        public void setMainInterfaceEnabled(bool enabled)
+        {
+            
+        }
+
+        public long PluginId
+        {
+            get
+            {
+                return 26;
+            }
+        }
+
+        public String PluginName
+        {
+            get
+            {
+                return "Presentation Editor";
+            }
+        }
+
+        public String BrandingImageKey
+        {
+            get
+            {
+                return "PresentationEditor/BrandingImage";
+            }
+        }
+
+        public void createMenuBar(NativeMenuBar menu)
+        {
+
+        }
+
+        public String Location
+        {
+            get
+            {
+                return GetType().Assembly.Location;
+            }
+        }
+
+        public Version Version
+        {
+            get
+            {
+                return GetType().Assembly.GetName().Version;
+            }
+        }
+
+        public void sceneRevealed()
+        {
+
+        }
+    }
+}
