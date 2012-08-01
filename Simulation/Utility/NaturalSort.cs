f0a578ac6da7cbda3dce8baa51823bcb9ec4a1ab
Andrew Piper <threax@users.noreply.github.com>
2012-08-01 21:18:30 +0000

    Surrounded NaturalSort in a try catch since sometimes it is invalid.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@3432 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Simulation/Utility/NaturalSort.cs
+++ Simulation/Utility/NaturalSort.cs
@@ -10,89 +10,96 @@ namespace Medical
     {
         public static int CompareFunc(String s1, String s2)
         {
-            if (s1 == null)
+            try
             {
-                return 0;
-            }
-            if (s2 == null)
-            {
-                return 0;
-            }
-
-            int len1 = s1.Length;
-            int len2 = s2.Length;
-            int marker1 = 0;
-            int marker2 = 0;
-
-            // Walk through two the strings with two markers.
-            while (marker1 < len1 && marker2 < len2)
-            {
-                char ch1 = s1[marker1];
-                char ch2 = s2[marker2];
-
-                // Some buffers we can build up characters in for each chunk.
-                char[] space1 = new char[len1];
-                int loc1 = 0;
-                char[] space2 = new char[len2];
-                int loc2 = 0;
-
-                // Walk through all following characters that are digits or
-                // characters in BOTH strings starting at the appropriate marker.
-                // Collect char arrays.
-                do
+                if (s1 == null)
                 {
-                    space1[loc1++] = ch1;
-                    marker1++;
+                    return 0;
+                }
+                if (s2 == null)
+                {
+                    return 0;
+                }
 
-                    if (marker1 < len1)
+                int len1 = s1.Length;
+                int len2 = s2.Length;
+                int marker1 = 0;
+                int marker2 = 0;
+
+                // Walk through two the strings with two markers.
+                while (marker1 < len1 && marker2 < len2)
+                {
+                    char ch1 = s1[marker1];
+                    char ch2 = s2[marker2];
+
+                    // Some buffers we can build up characters in for each chunk.
+                    char[] space1 = new char[len1];
+                    int loc1 = 0;
+                    char[] space2 = new char[len2];
+                    int loc2 = 0;
+
+                    // Walk through all following characters that are digits or
+                    // characters in BOTH strings starting at the appropriate marker.
+                    // Collect char arrays.
+                    do
                     {
-                        ch1 = s1[marker1];
+                        space1[loc1++] = ch1;
+                        marker1++;
+
+                        if (marker1 < len1)
+                        {
+                            ch1 = s1[marker1];
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    } while (char.IsDigit(ch1) == char.IsDigit(space1[0]));
+
+                    do
+                    {
+                        space2[loc2++] = ch2;
+                        marker2++;
+
+                        if (marker2 < len2)
+                        {
+                            ch2 = s2[marker2];
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    } while (char.IsDigit(ch2) == char.IsDigit(space2[0]));
+
+                    // If we have collected numbers, compare them numerically.
+                    // Otherwise, if we have strings, compare them alphabetically.
+                    string str1 = new string(space1);
+                    string str2 = new string(space2);
+
+                    int result;
+
+                    if (char.IsDigit(space1[0]) && char.IsDigit(space2[0]))
+                    {
+                        int thisNumericChunk = NumberParser.ParseInt(str1);
+                        int thatNumericChunk = NumberParser.ParseInt(str2);
+                        result = thisNumericChunk.CompareTo(thatNumericChunk);
                     }
                     else
                     {
-                        break;
+                        result = str1.CompareTo(str2);
                     }
-                } while (char.IsDigit(ch1) == char.IsDigit(space1[0]));
 
-                do
-                {
-                    space2[loc2++] = ch2;
-                    marker2++;
-
-                    if (marker2 < len2)
+                    if (result != 0)
                     {
-                        ch2 = s2[marker2];
+                        return result;
                     }
-                    else
-                    {
-                        break;
-                    }
-                } while (char.IsDigit(ch2) == char.IsDigit(space2[0]));
-
-                // If we have collected numbers, compare them numerically.
-                // Otherwise, if we have strings, compare them alphabetically.
-                string str1 = new string(space1);
-                string str2 = new string(space2);
-
-                int result;
-
-                if (char.IsDigit(space1[0]) && char.IsDigit(space2[0]))
-                {
-                    int thisNumericChunk = NumberParser.ParseInt(str1);
-                    int thatNumericChunk = NumberParser.ParseInt(str2);
-                    result = thisNumericChunk.CompareTo(thatNumericChunk);
                 }
-                else
-                {
-                    result = str1.CompareTo(str2);
-                }
-
-                if (result != 0)
-                {
-                    return result;
-                }
+                return len1 - len2;
             }
-            return len1 - len2;
+            catch (Exception)
+            {
+                return 0;
+            }
         }
     }
 
