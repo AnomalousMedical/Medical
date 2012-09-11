103e43552305a7142f7ebe76c9a2cb6972c97a4f
Andrew Piper <threax@users.noreply.github.com>
2012-09-11 16:46:03 +0000

    Made osx installer builders also sign the app bundle.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@3532 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- AnomalousMedical/Installer/OSX/Internal/InternalDMG.sh
+++ AnomalousMedical/Installer/OSX/Internal/InternalDMG.sh
@@ -6,4 +6,10 @@
 # Created by Andrew Piper on 1/18/11.
 # Copyright 2011 Anomalous Software. All rights reserved.
 
+codesign -s "Developer ID Application" ../../../../Release/Anomalous\ Medical.app
+
 sh ../MakeDMG.sh "Anomalous Medical" "../../../../Release" "Anomalous Medical" "../Layout" "../../License"
+
+rm AnomalousMedical.dmg
+
+mv Anomalous\ Medical.dmg AnomalousMedical.dmg
--- AnomalousMedical/Installer/OSX/Public/PublicDMG.sh
+++ AnomalousMedical/Installer/OSX/Public/PublicDMG.sh
@@ -6,4 +6,10 @@
 # Created by Andrew Piper on 1/18/11.
 # Copyright 2011 Anomalous Software. All rights reserved.
 
+codesign -s "Developer ID Application" ../../../../PublicRelease/Anomalous\ Medical.app
+
 sh ../MakeDMG.sh "Anomalous Medical" "../../../../PublicRelease" "Anomalous Medical" "../Layout" "../../License"
+
+rm AnomalousMedical.dmg
+
+mv Anomalous\ Medical.dmg AnomalousMedical.dmg
