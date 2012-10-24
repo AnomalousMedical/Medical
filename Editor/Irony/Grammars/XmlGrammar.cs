636e0168cb7c41f081a050c6415e739ef8ea76c0
Andrew Piper <threax@users.noreply.github.com>
2012-10-24 21:14:04 +0000

    Moved the key terms and used the MarkTransient and MarkPunctuation methods to clean up the parse tree a bit.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@3611 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Editor/Irony/Grammars/XmlGrammar.cs
+++ Editor/Irony/Grammars/XmlGrammar.cs
@@ -18,6 +18,14 @@ namespace Medical.Irony
             var number = new NumberLiteral("number");
             var stringLiteral = new StringLiteral("string", "\"", StringOptions.None);
             var stringContent = new XmlContentText("StringContent");
+            KeyTerm elementOpener = ToTerm("<");
+            KeyTerm closeElementOpener = ToTerm("</");
+            KeyTerm elementCloser = ToTerm(">");
+            KeyTerm openCloseElementCloser = ToTerm("/>");
+            KeyTerm equals = ToTerm("=");
+            KeyTerm xmlDeclOpen = ToTerm("<?");
+            KeyTerm xmlDeclClose = ToTerm("?>");
+
             IdentifierTerminal identifier = new IdentifierTerminal("Identifier");
 
             //Non Terminals
@@ -26,34 +34,37 @@ namespace Medical.Irony
             NonTerminal elementEnd = new NonTerminal("ElementEnd");
             NonTerminal openCloseElement = new NonTerminal("OpenCloseElement");
             NonTerminal element = new NonTerminal("Element");
-            NonTerminal requiredElements = new NonTerminal("Elements");
-            NonTerminal content = new NonTerminal("ElementContent");
-            NonTerminal multiContent = new NonTerminal("MultiContent");
+            NonTerminal requiredElements = new NonTerminal("RequiredElements");
+            NonTerminal innerContent = new NonTerminal("InnerContent");
+            NonTerminal content = new NonTerminal("Content");
             NonTerminal attribute = new NonTerminal("Attribute");
-            NonTerminal optionalAttribute = new NonTerminal("Attribute");
+            NonTerminal optionalAttribute = new NonTerminal("OptionalAttribute");
             NonTerminal xmlDeclaration = new NonTerminal("XmlDeclaration");
-            NonTerminal optionalXmlDeclaration = new NonTerminal("XmlDeclaration");
+            NonTerminal optionalXmlDeclaration = new NonTerminal("OptionalXmlDeclaration");
 
             //Rules
             this.Root = document;
 
-            content.Rule = element | stringContent;
-            multiContent.Rule = MakeStarRule(multiContent, content);
+            innerContent.Rule = element | stringContent;
+            content.Rule = MakeStarRule(content, innerContent);
 
-            attribute.Rule = identifier + ToTerm("=") + stringLiteral;
+            attribute.Rule = identifier + equals + stringLiteral;
             optionalAttribute.Rule = MakeStarRule(optionalAttribute, attribute);
 
-            elementStart.Rule = ToTerm("<") + identifier + optionalAttribute + ToTerm(">");
-            elementEnd.Rule = ToTerm("</") + identifier + ToTerm(">");
-            openCloseElement.Rule = ToTerm("<") + identifier + optionalAttribute + ToTerm("/>");
+            elementStart.Rule = elementOpener + identifier + optionalAttribute + elementCloser;
+            elementEnd.Rule = closeElementOpener + identifier + elementCloser;
+            openCloseElement.Rule = elementOpener + identifier + optionalAttribute + openCloseElementCloser;
 
-            element.Rule = (elementStart + multiContent + elementEnd) | openCloseElement;
+            element.Rule = (elementStart + content + elementEnd) | openCloseElement;
             requiredElements.Rule = MakePlusRule(requiredElements, element);
 
-            xmlDeclaration.Rule = ToTerm("<?") + identifier + optionalAttribute + ToTerm("?>");
+            xmlDeclaration.Rule = xmlDeclOpen + identifier + optionalAttribute + xmlDeclClose;
             optionalXmlDeclaration.Rule = MakeStarRule(optionalXmlDeclaration, xmlDeclaration);
 
             document.Rule = optionalXmlDeclaration + requiredElements;
+
+            MarkPunctuation(elementOpener, elementCloser, closeElementOpener, openCloseElementCloser, equals, xmlDeclOpen, xmlDeclClose);
+            MarkTransient(innerContent);
         }
 
     }
