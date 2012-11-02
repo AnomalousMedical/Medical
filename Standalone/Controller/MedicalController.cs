322ff9b38884776ce812fbafa182cf94ae64177c
Andrew Piper <threax@users.noreply.github.com>
2012-11-02 15:41:25 +0000

    Made a proof of concept that the ui can be scaled pretty easily with librocket just by preprocessing. need to figure out how to make the editor aware of pf extensions though.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@3642 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Standalone/Controller/MedicalController.cs
+++ Standalone/Controller/MedicalController.cs
@@ -118,6 +118,9 @@ namespace Medical
             BulletInterface.Instance.ShapeMargin = 0.005f;
             systemTimer = pluginManager.PlatformPlugin.createTimer();
 
+            RocketInterface.Instance.FileInterface = new RmlPreprocessStream();
+            RmlPreprocessStream.ScaleFactor = 1.0f;
+
             NativeUpdateTimer win32Timer = new NativeUpdateTimer(systemTimer);
             mainTimer = win32Timer;
             
