f07ef8ef09c9a99b6b36a24608f14599a94e8baf
Andrew Piper <threax@users.noreply.github.com>
2012-11-09 13:53:13 +0000

    Added a plugin for the leap motion.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@3647 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ LeapMotionPlugin/LeapAtlasPlugin.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Medical;
+using Medical.GUI;
+using Engine.ObjectManagement;
+
+namespace LeapMotionPlugin
+{
+    class LeapAtlasPlugin : AtlasPlugin
+    {
+
+        public LeapAtlasPlugin(StandaloneController standaloneController)
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public void loadGUIResources()
+        {
+            
+        }
+
+        public void initialize(StandaloneController standaloneController)
+        {
+            
+        }
+
+        public void sceneLoaded(SimScene scene)
+        {
+            
+        }
+
+        public void sceneRevealed()
+        {
+
+        }
+
+        public void sceneUnloading(SimScene scene)
+        {
+            
+        }
+
+        public void setMainInterfaceEnabled(bool enabled)
+        {
+
+        }
+
+        public long PluginId
+        {
+            get
+            {
+                return -1;
+            }
+        }
+
+        public String PluginName
+        {
+            get
+            {
+                return "Leap Motion Plugin";
+            }
+        }
+
+        public String BrandingImageKey
+        {
+            get
+            {
+                return CommonResources.NoIcon;
+            }
+        }
+
+        public String Location
+        {
+            get
+            {
+                return GetType().Assembly.Location;
+            }
+        }
+
+        public Version Version
+        {
+            get
+            {
+                return GetType().Assembly.GetName().Version;
+            }
+        }
+    }
+}
---
+++ LeapMotionPlugin/LeapAtlasPluginEntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Medical;
+
+[assembly: LeapMotionPlugin.LeapAtlasPluginEntryPoint()]
+
+namespace LeapMotionPlugin
+{
+    class LeapAtlasPluginEntryPoint : AtlasPluginEntryPointAttribute
+    {
+        public override void createPlugin(StandaloneController standaloneController)
+        {
+            standaloneController.AtlasPluginManager.addPlugin(new LeapAtlasPlugin(standaloneController));
+        }
+    }
+}
