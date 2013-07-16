839847682a67013876ebaa4f2985c3cd243fdc43
Andrew Piper <threax@users.noreply.github.com>
2013-07-16 18:37:43 +0000

    Added reference to Security.Framework. Added a function to validate ssl certificates themselves instead of making an extra call to the server.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@3965 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- OSHelper/Src/ConnectionValidator.mm
+++ OSHelper/Src/ConnectionValidator.mm
@@ -13,3 +13,68 @@ extern "C" _AnomalousExport bool ConnectionValidator_ValidateUrl(String url)
     [request release];
     return success;
 }
+
+OSStatus EvaluateCert (SecCertificateRef cert, CFTypeRef policyRef, SecTrustResultType *result, SecTrustRef *pTrustRef)
+{
+    OSStatus status1;
+    OSStatus status2;
+    
+    const void* evalCertArray[] = { cert };
+    CFArrayRef cfCertRef = CFArrayCreate(kCFAllocatorDefault, evalCertArray, 1, NULL);
+    
+    if (!cfCertRef)
+        return memFullErr;
+    
+    status1 = SecTrustCreateWithCertificates(cfCertRef, policyRef, pTrustRef);
+    if (status1)
+        return status1;
+    
+    status2 = SecTrustEvaluate (*pTrustRef, result);
+    
+    // Release the objects we allocated
+    if (cfCertRef)
+        CFRelease(cfCertRef);
+    //if (cfDate)
+    //    CFRelease(cfDate);
+    
+    return (status2);
+}
+
+bool CertificateValidator_ValidateSSLCertificate(unsigned char* certBytes, unsigned int bytesLength, const char* hostName)
+{
+    SecTrustRef trustRef = nil;
+    SecTrustResultType result = kSecTrustResultRecoverableTrustFailure;
+    bool success = false;
+    
+    CFStringRef cfHostName = NULL;
+    if(hostName != NULL)
+    {
+        cfHostName = CFStringCreateWithCString(kCFAllocatorDefault, hostName, kCFStringEncodingASCII);
+    }
+    
+    NSData *certData = [NSData dataWithBytes:certBytes length:bytesLength];
+    SecCertificateRef cert = SecCertificateCreateWithData(NULL, (CFDataRef)certData);
+    if(cert != nil)
+    {
+        SecPolicyRef policyRef = SecPolicyCreateSSL(false, cfHostName);
+        EvaluateCert(cert, policyRef, &result, &trustRef);
+        success = result == kSecTrustResultUnspecified;
+        
+        if(trustRef != nil)
+        {
+            CFRelease(trustRef);
+        }
+        
+        CFRelease(policyRef);
+    }
+    
+    if(cfHostName != NULL)
+    {
+        CFRelease(cfHostName);
+    }
+    
+    CFRelease(cert);
+    CFRelease(certData);
+    
+    return success;
+}
