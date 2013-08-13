0dce269373aaabda093b02526627fb7810966dab
Andrew Piper <threax@users.noreply.github.com>
2013-08-13 13:34:58 +0000

    Made it so the logos resize maintaining their aspect ratio.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@3999 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- StoreManager/Util/LogoUtil.cs
+++ StoreManager/Util/LogoUtil.cs
@@ -12,6 +12,24 @@ namespace Anomalous.Medical.StoreManager.Util
     {
         public static void SaveResizedImage(Bitmap source, Stream stream, int width, int height)
         {
+            SaveResizedImage(source, stream, ref width, ref height);
+        }
+
+        public static void SaveResizedImage(Bitmap source, Stream stream, ref int width, ref int height)
+        {
+            if (source.Width > source.Height)
+            {
+                float ratio = (float)width / (float)source.Width;
+                width = (int)(source.Width * ratio);
+                height = (int)(source.Height * ratio);
+            }
+            else if(source.Width < source.Height)
+            {
+                float ratio = (float)height / (float)source.Height;
+                width = (int)(source.Width * ratio);
+                height = (int)(source.Height * ratio);
+            }
+
             using (Bitmap resized = new Bitmap(width, height))
             {
                 using (Graphics graphics = Graphics.FromImage(resized))
