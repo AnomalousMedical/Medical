9f9e77ed953c1e479380b8d2d21e25f6a2e34377
Andrew Piper <threax@users.noreply.github.com>
2013-12-23 13:57:24 +0000

    Added a layout container that will force a certain aspect ratio on its children.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@4275 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ Standalone/Layout/AspectRatioContainer.cs
@@ -0,0 +1,131 @@
+﻿using Engine;
+using MyGUIPlugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Medical
+{
+    public class AspectRatioContainer : NullLayoutContainer, IDisposable
+    {
+        private float inverseAspectRatio = 1.0f;
+
+        private bool autoAspectRatio = true;
+        private Widget borderPanel0;
+        private Widget borderPanel1;
+
+        public AspectRatioContainer()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            destroyBorderPanels();
+        }
+
+        public override void layout()
+        {
+            if (Child != null)
+            {
+                IntVector2 sceneViewLocation = Location;
+                IntSize2 size = WorkingSize;
+
+                if (!autoAspectRatio)
+                {
+                    size.Height = (int)(size.Width * inverseAspectRatio);
+                    if (size.Height > WorkingSize.Height) //Letterbox width
+                    {
+                        size.Height = WorkingSize.Height;
+                        size.Width = (int)(size.Height * (1 / inverseAspectRatio));
+                        sceneViewLocation.x += (WorkingSize.Width - size.Width) / 2;
+
+                        borderPanel0.setCoord(Location.x, Location.y, sceneViewLocation.x - Location.x, WorkingSize.Height);
+                        borderPanel1.setCoord(sceneViewLocation.x + size.Width, Location.y, sceneViewLocation.x - Location.x + 1, WorkingSize.Height);
+                    }
+                    else
+                    {
+                        sceneViewLocation.y += (WorkingSize.Height - size.Height) / 2;
+
+                        borderPanel0.setCoord(Location.x, Location.y, WorkingSize.Width, sceneViewLocation.y - Location.y);
+                        borderPanel1.setCoord(Location.x, sceneViewLocation.y + size.Height, WorkingSize.Width, sceneViewLocation.y - Location.y + 1);
+                    }
+                }
+
+                Child.Location = sceneViewLocation;
+                Child.WorkingSize = size;
+                Child.layout();
+            }
+        }
+
+        public bool AutoAspectRatio
+        {
+            get
+            {
+                return autoAspectRatio;
+            }
+            set
+            {
+                if (autoAspectRatio != value)
+                {
+                    autoAspectRatio = value;
+                    if (autoAspectRatio)
+                    {
+                        destroyBorderPanels();
+                    }
+                    else
+                    {
+                        createBorderPanels();
+                    }
+                }
+            }
+        }
+
+        public float AspectRatio
+        {
+            get
+            {
+                if (inverseAspectRatio != 0.0f)
+                {
+                    return 1.0f / inverseAspectRatio;
+                }
+                else
+                {
+                    return 1.0f;
+                }
+            }
+            set
+            {
+                if (value != 0.0f)
+                {
+                    inverseAspectRatio = 1.0f / value;
+                }
+                else
+                {
+                    inverseAspectRatio = 1.0f;
+                }
+            }
+        }
+
+        void createBorderPanels()
+        {
+            if (borderPanel0 == null)
+            {
+                borderPanel0 = Gui.Instance.createWidgetT("Widget", "SceneViewBorder", 0, 0, 1, 1, Align.Default, "Back", "");
+                borderPanel1 = Gui.Instance.createWidgetT("Widget", "SceneViewBorder", 0, 0, 1, 1, Align.Default, "Back", "");
+            }
+        }
+
+        void destroyBorderPanels()
+        {
+            if (borderPanel0 != null)
+            {
+                Gui.Instance.destroyWidget(borderPanel0);
+                Gui.Instance.destroyWidget(borderPanel1);
+                borderPanel0 = null;
+                borderPanel1 = null;
+            }
+        }
+    }
+}
