051d3aface73dde3da8babaa64a897c9a8cc5f9c
Andrew Piper <threax@users.noreply.github.com>
2014-01-13 19:13:34 +0000

    Modified the wildcard discovery on osx to support multiple extensions per entry.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@4304 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- OSHelper/Src/CocoaDialogs.mm
+++ OSHelper/Src/CocoaDialogs.mm
@@ -6,6 +6,36 @@
 
 #include <Cocoa/Cocoa.h>
 
+//Process an entry for wildcards, returns true if the wildcard matches everything
+bool processEntry(NSString* entry, NSMutableArray* fileTypeVector)
+{
+    NSUInteger dotPos = [entry rangeOfString:@"."].location;
+    if(dotPos != NSNotFound)
+    {
+        //Consider the whole string the filter, from the . to the end
+        NSString* card = [entry substringFromIndex:dotPos + 1];
+        if([card isEqualToString: @"*"])
+        {
+            return true;
+        }
+        [fileTypeVector addObject:card];
+    }
+    return false;
+}
+
+bool processEntries(NSString* entry, NSMutableArray* fileTypeVector)
+{
+    NSArray* fileTypes = [entry componentsSeparatedByString:@";"];
+    for(NSUInteger i = 0; i < [fileTypes count]; ++i)
+    {
+        if(processEntry([fileTypes objectAtIndex:i], fileTypeVector))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 //Wildcard, these are in the format description|extension|description|extension
 //Will return true if the filters should be used and false if not.
 bool convertWildcards(String utf16Wildcard, NSMutableArray* fileTypeVector)
@@ -15,35 +45,18 @@ bool convertWildcards(String utf16Wildcard, NSMutableArray* fileTypeVector)
     {
         return false;
     }
-    NSUInteger dotPos = 0;
     NSArray* fileTypes = [wildcard componentsSeparatedByString:@"|"];
 	if([fileTypes count] == 1)
 	{
-        dotPos = [wildcard rangeOfString:@"."].location;
-        if(dotPos != NSNotFound)
-        {
-            //Consider the whole string the filter, from the . to the end
-            NSString* card = [wildcard substringFromIndex:dotPos + 1];
-            if([card isEqualToString: @"*"])
-            {
-                return false;
-            }
-            [fileTypeVector addObject:card];
-        }		
+        return !processEntries(wildcard, fileTypeVector);
 	}
 	else
 	{
 		for(NSUInteger i = 1; i < [fileTypes count]; i += 2)
         {
-            dotPos = [[fileTypes objectAtIndex:i] rangeOfString:@"."].location;
-            if(dotPos != NSNotFound)
+            if(processEntries([fileTypes objectAtIndex:i], fileTypeVector))
             {
-                NSString* card = [[fileTypes objectAtIndex:i] substringFromIndex:dotPos + 1];
-                if([card isEqualToString: @"*"])
-                {
-                    return false;
-                }
-                [fileTypeVector addObject:card];
+                return false;
             }
         }
 	}
