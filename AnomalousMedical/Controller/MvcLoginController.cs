ec68864e9d998eae3b85990ca086f8d8a115ca40
Andrew Piper <threax@users.noreply.github.com>
2014-02-13 19:27:27 +0000

    Fixed order of closing the login screen, this way if the intro tutorial starts it is not ignored.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@4447 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- AnomalousMedical/Controller/MvcLoginController.cs
+++ AnomalousMedical/Controller/MvcLoginController.cs
@@ -145,8 +145,8 @@ namespace Medical.Controller
 
         void licenseCaptured()
         {
-            loginSucessfulCallback();
             this.close();
+            loginSucessfulCallback();
         }
 
         void licenseLoginFail()
