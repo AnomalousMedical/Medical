b9f1d64a4cca0969712fa9072463cdea3976a254
Andrew Piper <threax@users.noreply.github.com>
2014-03-13 18:59:41 +0000

    Removed the osx version check.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@4484 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- OSHelper/Src/SystemInfo.mm
+++ OSHelper/Src/SystemInfo.mm
@@ -23,38 +23,3 @@ extern "C" _AnomalousExport void SystemInfo_getDisplayLocation(int displayIndex,
         y = 0;
     }
 }
-
-extern "C" _AnomalousExport void SystemInfo_GetOSXVersion(int &major, int &minor, int &bugfix)
-{
-    // sensible default
-	static int mMajor = -1;
-	static int mMinor = 0;
-	static int mBugfix = 0;
-    
-	static dispatch_once_t onceToken;
-	dispatch_once(&onceToken, ^{
-        NSDictionary *systemVersionPlist = [NSDictionary dictionaryWithContentsOfFile:@"/System/Library/CoreServices/SystemVersion.plist"];
-        if(systemVersionPlist != nil)
-        {
-            NSString* versionString = [systemVersionPlist objectForKey:@"ProductVersion"];
-            NSArray* versions = [versionString componentsSeparatedByString:@"."];
-            check( versions.count >= 2 );
-            if ( versions.count >= 1 )
-            {
-                mMajor = [[versions objectAtIndex:0] integerValue];
-            }
-            if ( versions.count >= 2 )
-            {
-                mMinor = [[versions objectAtIndex:1] integerValue];
-            }
-            if ( versions.count >= 3 )
-            {
-                mBugfix = [[versions objectAtIndex:2] integerValue];
-            }
-        }
-	});
-    
-	major = mMajor;
-	minor = mMinor;
-	bugfix = mBugfix;
-}
