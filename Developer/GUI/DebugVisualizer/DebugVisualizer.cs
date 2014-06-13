b6698387a07943c6235f9c483c7cbe7bc37b3e59
Andrew Piper <threax@users.noreply.github.com>
2014-06-13 13:09:08 +0000

    Moved the GenericEditableProperty out of this class into engine, renamed it CallbackEditableProperty.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@4653 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Developer/GUI/DebugVisualizer/DebugVisualizer.cs
+++ Developer/GUI/DebugVisualizer/DebugVisualizer.cs
@@ -77,15 +77,15 @@ namespace Developer.GUI
                     propertyInfo.addColumn(new EditablePropertyColumn("Value", false));
                     debugEditInterface.setPropertyInfo(propertyInfo);
 
-                    debugEditInterface.addEditableProperty(new GenericEditableProperty<bool>("Enabled",
+                    debugEditInterface.addEditableProperty(new CallbackEditableProperty<bool>("Enabled",
                         () => debugInterface.Enabled, v => debugInterface.Enabled = v, canParseBool, bool.Parse));
 
-                    debugEditInterface.addEditableProperty(new GenericEditableProperty<bool>("Depth Testing",
+                    debugEditInterface.addEditableProperty(new CallbackEditableProperty<bool>("Depth Testing",
                         () => debugInterface.DepthTesting, v => debugInterface.DepthTesting = v, canParseBool, bool.Parse));
 
                     foreach(var entry in debugInterface.getEntries())
                     {
-                        debugEditInterface.addEditableProperty(new GenericEditableProperty<bool>(entry.Text,
+                        debugEditInterface.addEditableProperty(new CallbackEditableProperty<bool>(entry.Text,
                             () => entry.Enabled, value => entry.Enabled = value, canParseBool, bool.Parse));
                     }
                     
@@ -148,114 +148,6 @@ namespace Developer.GUI
             foreach (DebugInterface debugInterface in pluginManager.getDebugInterfaces())
             {
                 debugInterface.renderDebug(subScene);
-            }
-        }
-
-        //probably move this out to the engine
-        class GenericEditableProperty<T> : EditableProperty
-        {
-            private String name;
-            private Func<T> getGenericValue;
-            private Action<T> setGenericValue;
-            private Func<String, bool> canParseStringToValue;
-            private Func<String, T> parseStringToValue;
-
-            public GenericEditableProperty(String name, Func<T> getGenericValue, Action<T> setGenericValue, Func<String, bool> canParseStringToValue, Func<String, T> parseStringToValue)
-            {
-                this.name = name;
-                this.getGenericValue = getGenericValue;
-                this.setGenericValue = setGenericValue;
-                this.canParseStringToValue = canParseStringToValue;
-                this.parseStringToValue = parseStringToValue;
-            }
-
-            public bool Advanced
-            {
-                get
-                {
-                    return false;
-                }
-            }
-
-            public bool canParseString(int column, string value, out string errorMessage)
-            {
-                if(canParseStringToValue(value))
-                {
-                    errorMessage = null;
-                    return true;
-                }
-                errorMessage = "Cannot parse.";
-                return false;
-            }
-
-            public Browser getBrowser(int column, EditUICallback uiCallback)
-            {
-                return null;
-            }
-
-            public Type getPropertyType(int column)
-            {
-                switch (column)
-                {
-                    case 0:
-                        return typeof(String);
-                    case 1:
-                        return typeof(T);
-                    default:
-                        return typeof(String);
-                }
-            }
-
-            public object getRealValue(int column)
-            {
-                switch (column)
-                {
-                    case 0:
-                        return name;
-                    case 1:
-                        return getGenericValue();
-                    default:
-                        return null;
-                }
-            }
-
-            public string getValue(int column)
-            {
-                switch (column)
-                {
-                    case 0:
-                        return name;
-                    case 1:
-                        return getGenericValue().ToString();
-                    default:
-                        return null;
-                }
-            }
-
-            public bool hasBrowser(int column)
-            {
-                return false;
-            }
-
-            public bool readOnly(int column)
-            {
-                return column == 0;
-            }
-
-            public void setValue(int column, object value)
-            {
-                if (column == 1)
-                {
-                    setGenericValue((T)value);
-                }
-            }
-
-            public void setValueStr(int column, string value)
-            {
-                if (column == 1)
-                {
-                    setGenericValue(parseStringToValue(value));
-                }
             }
         }
     }
