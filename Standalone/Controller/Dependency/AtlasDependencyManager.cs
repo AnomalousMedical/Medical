0f30d8ce9a1a5dd6e691bcf872aad2ad8b837b12
Andrew Piper <threax@users.noreply.github.com>
2014-08-01 15:07:55 +0000

    Initial dependency system, not really hooked up to anything yet, mostly a clone of the plugin system for now.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@4781 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ Standalone/Controller/Dependency/AtlasDependencyManager.cs
@@ -0,0 +1,171 @@
+﻿using Engine;
+using Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical
+{
+    public class AtlasDependencyManager : IDisposable
+    {
+        public delegate void DependencyMessageDelegate(String message);
+        public event DependencyMessageDelegate LoadError;
+
+        private StandaloneController standaloneController;
+        private Dictionary<long, AtlasDependency> dependencies = new Dictionary<long, AtlasDependency>();
+        private String additionalSearchPath;
+        private HashSet<String> loadedDependencyNames = new HashSet<string>();
+
+        public AtlasDependencyManager(StandaloneController standaloneController)
+        {
+            this.standaloneController = standaloneController;
+            additionalSearchPath = FolderFinder.ExecutableFolder;
+        }
+
+        public void Dispose()
+        {
+            foreach(var dependency in dependencies.Values)
+            {
+                dependency.Dispose();
+            }
+        }
+
+        public bool addDependency(String path)
+        {
+            //Do type dll or data driven check here
+
+            return addDataDrivenDependency(path);
+        }
+
+        private bool addDataDrivenDependency(String path)
+        {
+            bool loadedDependency = false;
+            String dependencyDirectory = null;
+            String fullPath = Path.GetFullPath(path);
+            if (!Directory.Exists(fullPath) && !File.Exists(fullPath))
+            {
+                fullPath = Path.Combine(additionalSearchPath, path);
+            }
+
+            //put file load code here
+
+            if (Directory.Exists(fullPath))
+            {
+                String directoryName = Path.GetFileName(Path.GetDirectoryName(fullPath));
+                if (!loadedDependencyNames.Contains(directoryName))
+                {
+                    loadedDependencyNames.Add(directoryName);
+
+                    dependencyDirectory = String.Format("Dependencies/{0}/", directoryName);
+                    String rootPath = fullPath.Replace("\\", "/");
+                    if (!rootPath.EndsWith("/"))
+                    {
+                        rootPath += "/";
+                    }
+                    rootPath = rootPath.Replace(dependencyDirectory, "");
+
+                    //Add the archive to the VirtualFileSystem if needed
+                    if (!VirtualFileSystem.Instance.containsRealAbsolutePath(rootPath))
+                    {
+                        VirtualFileSystem.Instance.addArchive(rootPath);
+                    }
+                }
+                else
+                {
+                    Log.Error("Cannot load data file '{0}' from '{1}' because a dependency named '{2}' is already loaded.", path, fullPath, directoryName);
+                }
+            }
+            else
+            {
+                Log.Error("Cannot load data file '{0}' from '{0}' or '{1}' because it was not found.", path, fullPath, Path.GetFullPath(path));
+            }
+
+            if(dependencyDirectory != null)
+            {
+                String definitionFile = dependencyDirectory + "Dependency.ddd";
+
+                if (VirtualFileSystem.Instance.exists(definitionFile))
+                {
+                    using (Stream stream = VirtualFileSystem.Instance.openStream(definitionFile, Engine.Resources.FileMode.Open, Engine.Resources.FileAccess.Read))
+                    {
+                        try
+                        {
+                            DDAtlasDependency plugin = SharedXmlSaver.Load<DDAtlasDependency>(stream);
+                            if (plugin != null)
+                            {
+                                plugin.Location = fullPath;
+                                plugin.RootFolder = dependencyDirectory;
+                                addDependency(plugin);
+                                loadedDependency = true;
+                            }
+                            else
+                            {
+                                throw new Exception(String.Format("Error loading '{0}' in path '{1}' from '{2}' because it was null.", definitionFile, path, fullPath));
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            fireLoadError(String.Format("There was an error loading the plugin '{0}'.", Path.GetFileName(fullPath)));
+                            Log.Error(ex.Message);
+                            try
+                            {
+                                File.Delete(fullPath);
+                            }
+                            catch (Exception deleteEx)
+                            {
+                                Log.Error("Error deleting data file '{0}' from '{1}' because: {2}.", path, fullPath, deleteEx.Message);
+                                //managePluginInstructions.addFileToDelete(fullPath);
+                                //saveManagementInstructions();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Log.Error("Error loading '{0}' in path '{1}' from '{2}' because it does not exist.", definitionFile, path, fullPath);
+                }
+            }
+
+            return loadedDependency;
+        }
+
+        public void addDependency(AtlasDependency dependency)
+        {
+            if(!dependencies.ContainsKey(dependency.DependencyId))
+            {
+                dependencies.Add(dependency.DependencyId, dependency);
+            }
+            else
+            {
+                Log.Error("Attempted to add Dependency '{0}' with id '{1}' that is already in use for another dependency named '{2}'", dependency.Name, dependency.DependencyId, dependencies[dependency.DependencyId].Name);
+            }
+        }
+
+        public void initializeDependency(long dependencyId)
+        {
+            AtlasDependency dependency;
+            if (dependencies.TryGetValue(dependencyId, out dependency))
+            {
+                if(!dependency.Initialized)
+                {
+                    dependency.initialize(standaloneController);
+                }
+            }
+            else
+            {
+                Log.Warning("Attempted to initialize dependency id {0}, but it has not been loaded.", dependencyId);
+            }
+        }
+
+        private void fireLoadError(String message)
+        {
+            if (LoadError != null)
+            {
+                LoadError.Invoke(message);
+            }
+        }
+    }
+}
---
+++ Standalone/Controller/Dependency/DDAtlasDependency.cs
@@ -0,0 +1,88 @@
+﻿using Engine.Editing;
+using Engine.Saving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical
+{
+    public class DDAtlasDependency : AtlasDependency, Saveable
+    {
+        public DDAtlasDependency()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void loadGUIResources()
+        {
+            
+        }
+
+        public void initialize(StandaloneController standaloneController)
+        {
+            Initialized = true;
+        }
+
+        [Editable]
+        public long DependencyId { get; set; }
+
+        [Editable]
+        public string Name { get; set; }
+
+        [Editable]
+        public string BrandingImageKey { get; set; }
+
+        public string Location { get; internal set; }
+
+        public Version Version { get; private set; }
+
+        [Editable]
+        public String VersionString
+        {
+            get
+            {
+                return Version.ToString();
+            }
+            set
+            {
+                try
+                {
+                    Version = new Version(value);
+                }
+                catch (Exception) { }
+            }
+        }
+
+        public String RootFolder { get; internal set; }
+
+        [Editable]
+        public String DependencyNamespace { get; set; }
+
+        public bool Initialized { get; private set; }
+
+        protected DDAtlasDependency(LoadInfo info)
+        {
+            DependencyId = info.GetInt64("DependencyId");
+            Name = info.GetString("Name");
+            BrandingImageKey = info.GetString("BrandingImageKey");
+            VersionString = info.GetString("VersionString");
+            DependencyNamespace = info.GetString("DependencyNamespace");
+        }
+
+        public void getInfo(SaveInfo info)
+        {
+            info.AddValue("DependencyId", DependencyId);
+            info.AddValue("Name", Name);
+            info.AddValue("BrandingImageKey", BrandingImageKey);
+            info.AddValue("VersionString", VersionString);
+            info.AddValue("DependencyNamespace", DependencyNamespace);
+        }
+    }
+}
