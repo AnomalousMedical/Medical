19ba92a06608c2a47a39b3e3f8008704948bdfb4
Andrew Piper <threax@users.noreply.github.com>
2014-09-22 13:56:58 +0000

    Added a project for movement plugin.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@4966 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ Movement/MovementBodyAtlasPlugin.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Engine.ObjectManagement;
+using MyGUIPlugin;
+using Medical.Controller;
+using System.Diagnostics;
+using Medical.GUI;
+using Engine;
+
+namespace Medical
+{
+    public class MovementBodyAtlasPlugin : AtlasPlugin
+    {
+        private StandaloneController standaloneController;
+
+        public MovementBodyAtlasPlugin()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void loadGUIResources()
+        {
+            //ResourceManager.Instance.load("Medical.Resources.PremiumImagesets.xml");
+        }
+
+        public void initialize(StandaloneController standaloneController)
+        {
+            
+        }
+
+        public void sceneLoaded(SimScene scene)
+        {
+            
+        }
+
+        public void sceneUnloading(SimScene scene)
+        {
+            
+        }
+
+        public void setMainInterfaceEnabled(bool enabled)
+        {
+            
+        }
+
+        public void sceneRevealed()
+        {
+            
+        }
+
+        public long PluginId
+        {
+            get
+            {
+                return -1;
+            }
+        }
+
+        public String PluginName
+        {
+            get
+            {
+                return "Movement Simulation";
+            }
+        }
+
+        public String BrandingImageKey
+        {
+            get
+            {
+                return CommonResources.NoIcon;
+            }
+        }
+
+        public String Location
+        {
+            get
+            {
+                return GetType().Assembly.Location;
+            }
+        }
+
+        public Version Version
+        {
+            get
+            {
+                return GetType().Assembly.GetName().Version;
+            }
+        }
+
+        public bool AllowUninstall
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public IEnumerable<long> DependencyPluginIds
+        {
+            get
+            {
+                return IEnumerableUtil<long>.EmptyIterator;
+            }
+        }
+    }
+}
---
+++ Movement/MovementPluginEntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[assembly: Medical.MovementPluginEntryPoint()]
+
+namespace Medical
+{
+    class MovementPluginEntryPoint : AtlasPluginEntryPointAttribute
+    {
+        public override void createPlugin(StandaloneController standaloneController)
+        {
+            standaloneController.AtlasPluginManager.addPlugin(new MovementBodyAtlasPlugin());
+        }
+    }
+}
