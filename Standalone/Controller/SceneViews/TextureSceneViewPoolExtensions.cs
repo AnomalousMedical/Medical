8728b64add5ce2f0d92162ce0392a09fa1e60ac1
Andrew Piper <threax@users.noreply.github.com>
2014-12-31 16:35:14 +0000

    Updated TextureSceneViewPool to work from the GuiFramework.Cameras library the part with the LayerStates is now an extension method in Medical.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@5349 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ Standalone/Controller/SceneViews/TextureSceneViewPoolExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Anomalous.GuiFramework.Cameras;
+using Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical
+{
+    public static class TextureSceneViewPoolExtensions
+    {
+        public static PooledSceneView getSceneView(this TextureSceneViewPool pool, Vector3 translation, Vector3 lookAt, LayerState layers)
+        {
+            PooledSceneView pooledView = pool.getSceneView(translation, lookAt);
+
+            layers.instantlyApplyTo(pooledView.SceneView.CurrentTransparencyState);
+
+            return pooledView;
+        }
+    }
+}
