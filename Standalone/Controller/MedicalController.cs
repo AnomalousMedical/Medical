fbc706a28c123c45ba61a34e8a666f02c725e308
Andrew Piper <threax@users.noreply.github.com>
2014-12-31 17:04:53 +0000

    Converted to use events instead of the TransparencyControllerShim.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@5351 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- Standalone/Controller/MedicalController.cs
+++ Standalone/Controller/MedicalController.cs
@@ -118,7 +118,8 @@ namespace Medical
             CamerasInterface.SelectWindowEventLayer = EventLayers.AfterGui;
             CamerasInterface.TouchType = PlatformConfig.TouchType;
             CamerasInterface.PanKey = PlatformConfig.PanKey;
-            CamerasInterface.TransparencyController = new TransparencyControllerShim();
+            CamerasInterface.WindowCreated += CamerasInterface_WindowCreated;
+            CamerasInterface.WindowDestroyed += CamerasInterface_WindowDestroyed;
 
             pluginManager.addPluginAssembly(typeof(OgreInterface).Assembly);
             pluginManager.addPluginAssembly(typeof(BulletInterface).Assembly);
@@ -346,6 +347,30 @@ namespace Medical
             {
                 return pluginManager;
             }
+        }
+
+        void CamerasInterface_WindowDestroyed(SceneViewWindow window)
+        {
+            TransparencyController.removeTransparencyState(window.CurrentTransparencyState);
+            window.RenderingStarted -= window_RenderingStarted;
+            window.MadeActive -= window_MadeActive;
+        }
+
+        void CamerasInterface_WindowCreated(SceneViewWindow window)
+        {
+            TransparencyController.createTransparencyState(window.CurrentTransparencyState);
+            window.RenderingStarted += window_RenderingStarted;
+            window.MadeActive += window_MadeActive;
+        }
+
+        void window_MadeActive(SceneViewWindow window)
+        {
+            TransparencyController.ActiveTransparencyState = window.CurrentTransparencyState;
+        }
+
+        void window_RenderingStarted(SceneViewWindow window, bool currentCameraRender)
+        {
+            TransparencyController.applyTransparencyState(window.CurrentTransparencyState);
         }
     }
 }
