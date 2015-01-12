782b9c8b99749b467a548fc74c5c1bb9e1f96610
Andrew Piper <threax@users.noreply.github.com>
2015-01-12 16:03:34 +0000

    Fixed path to sign tool.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@5383 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- AutoBuildInternal.bat
+++ AutoBuildInternal.bat
@@ -3,7 +3,7 @@ set SolutionName=Medical.sln
 
 ::Less likely to need to change these.
 set ThisFolder=%~dp0
-set RootDependencyFolder=%ThisFolder%..\
+set RootBuildFolder=%ThisFolder%..\
 set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" /m
 set CurrentDirectory=%CD%
 set InnosetupCommand="C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc
