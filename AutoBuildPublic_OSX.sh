193111dfec0c69f99fa1eb866829056a9fda0661
Andrew Piper <threax@users.noreply.github.com>
2015-01-14 14:57:21 +0000

    Added auto builds for internal and public anomalous medical. Does not do installer yet.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@5388 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ AutoBuildPublic_OSX.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#Discover some stuff about where we are.
+START_PATH=$(pwd)
+THIS_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#Configuration Settings
+SOLUTION_NAME=MedicalMac.sln
+
+#Do Build
+SOLUTION_PATH=$THIS_FOLDER/$SOLUTION_NAME
+
+#cleaning doesn't work for some reason
+xbuild "$SOLUTION_PATH" /p:Configuration=PublicRelease /t:clean
+xbuild "$SOLUTION_PATH" /p:Configuration=PublicRelease
+
+#Finish up
+cd "$START_PATH"
