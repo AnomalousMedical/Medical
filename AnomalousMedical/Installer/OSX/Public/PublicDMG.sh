64618fa7895da39bff9a5b8fb30c2ba24acfed4c
Andrew Piper <threax@users.noreply.github.com>
2015-01-14 15:10:16 +0000

    Made installer scripts more robust when run from other directories.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@5389 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- AnomalousMedical/Installer/OSX/Public/PublicDMG.sh
+++ AnomalousMedical/Installer/OSX/Public/PublicDMG.sh
@@ -6,6 +6,10 @@
 # Created by Andrew Piper on 1/18/11.
 # Copyright 2011 Anomalous Software. All rights reserved.
 
+THIS_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+pushd $THIS_FOLDER
+
 codesign -s "Developer ID Application" ../../../../PublicRelease/Anomalous\ Medical.app --deep
 
 sh ../MakeDMG.sh "Anomalous Medical" "../../../../PublicRelease" "Anomalous Medical" "../Layout" "../../License"
@@ -13,3 +17,5 @@ sh ../MakeDMG.sh "Anomalous Medical" "../../../../PublicRelease" "Anomalous Medi
 rm AnomalousMedical.dmg
 
 mv Anomalous\ Medical.dmg AnomalousMedical.dmg
+
+popd
