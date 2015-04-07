a47542641f29c78a9af62e36ea2ee8cfc3a82c7f
Andrew Piper <threax@users.noreply.github.com>
2015-04-07 20:54:50 +0000

    Updated medical to use AndroidActivity superclass.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@5524 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- AnomalousMedicalAndroid/MainActivity.cs
+++ AnomalousMedicalAndroid/MainActivity.cs
@@ -21,12 +21,13 @@ namespace AndroidBaseApp
 	[Activity (Label = "Anomalous Medical", MainLauncher = true, Icon = "@drawable/icon", Theme="@android:style/Theme.NoTitleBar.Fullscreen", 
 		ConfigurationChanges= ConfigChanges.Orientation | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize | ConfigChanges.ScreenLayout,
 		WindowSoftInputMode = SoftInput.StateAlwaysHidden)]
-	[MetaData("android.app.lib_name", Value="OSHelper")]
-	public class MainActivity : NativeActivity
+	[MetaData("android.app.lib_name", Value = "OSHelper")]
+	public class MainActivity : AndroidActivity
 	{
-		private EditText editText;
-		bool fireInputs = true;
-		private NativeInputHandler inputHandler;
+		static MainActivity()
+		{
+			Java.Lang.JavaSystem.LoadLibrary ("openal");
+		}
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -34,28 +35,23 @@ namespace AndroidBaseApp
 			Logging.Log.Default.addLogListener (new Logging.LogConsoleListener ());
 			#endif
 
-			Java.Lang.JavaSystem.LoadLibrary ("openal");
-
-			AndroidFunctions.EasyAttributeSetup (Resources.DisplayMetrics.Density, toggleKeyboard);
-
-			var anomalous = new AnomalousController();
-			anomalous.OnInitCompleted += HandleOnInitCompleted;
-			anomalous.run();
-
 			base.OnCreate (bundle);
 
 			SetContentView (AnomalousMedicalAndroid.Resource.Layout.Main);
+			setViews(FindViewById<EditText>(AnomalousMedicalAndroid.Resource.Id.editText1));
+		}
 
-			editText = FindViewById<EditText> (AnomalousMedicalAndroid.Resource.Id.editText1);
-			editText.TextChanged += HandleTextChanged;
-			Window.SetSoftInputMode (SoftInput.StateAlwaysHidden);
+		protected override void createApp ()
+		{
+			var anomalous = new AnomalousController();
+			anomalous.OnInitCompleted += HandleOnInitCompleted;
+			anomalous.run();
 		}
 
 		void HandleOnInitCompleted (AnomalousController anomalousController, StandaloneController controller)
 		{
-			inputHandler = controller.MedicalController.InputHandler;
+			setInputHandler(controller.MedicalController.InputHandler);
 
-			//String archivePath =  "/sdcard/AnomalousMedical.dat";
 			//Application.Context.FilesDir;
 			String archivePath = Path.Combine(Application.Context.ObbDir.AbsolutePath, "AnomalousMedical.dat");
 			if (System.IO.File.Exists (archivePath) || System.IO.Directory.Exists(archivePath)) 
@@ -66,60 +62,6 @@ namespace AndroidBaseApp
 			{
 				Logging.Log.Warning ("Cannot find primarydata file");
 			}
-		}
-
-		/// <summary>
-		/// Handle the text changing in our edit box. Note that this only really handles text and not
-		/// all keyboard events, other keyboard events like enter and delete(sometimes) will come from the 
-		/// native code input function.
-		/// </summary>
-		void HandleTextChanged (object sender, Android.Text.TextChangedEventArgs e)
-		{
-			if (fireInputs) 
-			{
-				//Logging.Log.Debug ("'{0}' bc: {2} ac: {1} s: {3}", e.Text.ToString (), e.AfterCount, e.BeforeCount, e.Start);
-				if (e.AfterCount - e.BeforeCount >= 0) 
-				{
-					foreach (char c in e.Text.Skip(e.Start + e.BeforeCount)) 
-					{
-						inputHandler.injectKeyPressed (Engine.Platform.KeyboardButtonCode.KC_UNASSIGNED, c);
-						inputHandler.injectKeyReleased (Engine.Platform.KeyboardButtonCode.KC_UNASSIGNED);
-					}
-				} 
-				else 
-				{
-					int count = e.BeforeCount - e.AfterCount;
-					for (int i = 0; i < count; ++i) 
-					{
-						inputHandler.injectKeyPressed (Engine.Platform.KeyboardButtonCode.KC_BACK, 0);
-						inputHandler.injectKeyReleased (Engine.Platform.KeyboardButtonCode.KC_BACK);
-					}
-				}
-			} 
-			//else 
-			//{
-			//	Logging.Log.Debug ("Clearing");
-			//}
-		}
-
-		void toggleKeyboard(bool visible)
-		{
-			RunOnUiThread (() => 
-				{
-					fireInputs = false;
-					editText.Text = "";
-					fireInputs = true;
-
-					InputMethodManager inputMethod = GetSystemService (InputMethodService) as InputMethodManager;
-					if (visible) 
-					{
-						inputMethod.ShowSoftInput (editText, ShowFlags.Forced);
-					}
-					else 
-					{
-						inputMethod.HideSoftInputFromWindow (editText.WindowToken, 0);
-					}
-				});
 		}
 	}
 }
