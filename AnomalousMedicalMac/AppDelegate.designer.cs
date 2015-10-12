4153e88dd89ec738d803e3f34acdc32c28622b01
Andrew Piper <threax@users.noreply.github.com>
2015-10-12 20:17:39 +0000

    Trying to use mono mac to create a debuggable project. Also modified ServerConnection to not disable tls 1.0 and not to enable 1.1 and 1.2.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@5865 956a8c8d-885e-cb46-a0b9-a2f355b4c364

---
+++ AnomalousMedicalMac/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AnomalousMedicalMac
+{
+    // Should subclass MonoMac.AppKit.NSResponder
+    [MonoMac.Foundation.Register("AppDelegate")]
+    public partial class AppDelegate
+    {
+    }
+}
+
---
+++ AnomalousMedicalMac/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+
+namespace AnomalousMedicalMac
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Medical.Main.Run();
+        }
+    }
+}
+
