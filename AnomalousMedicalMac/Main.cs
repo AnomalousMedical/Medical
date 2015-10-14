bb605d5c65a9cb335c47639eeb10794755229187
Andrew Piper <threax@users.noreply.github.com>
2015-10-14 13:17:44 +0000

    Added data files to mac version. Changed mac main to be fully written out and to specify the correct directory for the data file.
    git-svn-id: https://192.168.3.139:8443/svn/Medical/trunk@5895 956a8c8d-885e-cb46-a0b9-a2f355b4c364

--- AnomalousMedicalMac/Main.cs
+++ AnomalousMedicalMac/Main.cs
@@ -5,6 +5,8 @@ using MonoMac.ObjCRuntime;
 using Medical;
 using System.Net.Http;
 using MonoMac.HttpClient;
+using Anomalous.OSPlatform;
+using System.IO;
 
 namespace AnomalousMedicalMac
 {
@@ -16,7 +18,40 @@ namespace AnomalousMedicalMac
 
             ServerConnection.HttpClientProvider = () => new HttpClient(new NativeMessageHandler());
 
-            Medical.Main.Run();
+            NativePlatformPlugin.StaticInitialize();
+            OgrePlugin.OgreInterface.CompressedTextureSupport = OgrePlugin.CompressedTextureSupport.None;
+
+            AnomalousController anomalous = null;
+            try
+            {
+                anomalous = new AnomalousController()
+                    {
+                        PrimaryArchive = Path.Combine(FolderFinder.ExecutableFolder, "../Resources/AnomalousMedical.dat")
+                    };
+                anomalous.run();
+            }
+            catch (Exception e)
+            {
+                Logging.Log.Default.printException(e);
+                if (anomalous != null)
+                {
+                    anomalous.saveCrashLog();
+                }
+                String errorMessage = e.Message + "\n" + e.StackTrace;
+                while (e.InnerException != null)
+                {
+                    e = e.InnerException;
+                    errorMessage += "\n" + e.Message + "\n" + e.StackTrace;
+                }
+                MessageDialog.showErrorDialog(errorMessage, "Exception");
+            }
+            finally
+            {
+                if (anomalous != null)
+                {
+                    anomalous.Dispose();
+                }
+            }
         }
     }
 }
