3769eba5c62e9ecaaed3253ca0e9d37cd52f0d4c
Andrew Piper <threax@users.noreply.github.com>
2016-02-03 16:47:28 -0500

    Removed hard coded path from msbuild.

--- AutoBuildInternal.bat
+++ AutoBuildInternal.bat
@@ -4,7 +4,7 @@ set SolutionName=Medical.sln
 ::Less likely to need to change these.
 set ThisFolder=%~dp0
 set RootBuildFolder=%ThisFolder%..\
-set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" /m
+set BuildCommand="msbuild.exe" /m
 set CurrentDirectory=%CD%
 set InnosetupCommand="C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc
 set SignCommand=call "%RootBuildFolder%DRM\CodeKey\AnomalousMedicalSign.bat"
