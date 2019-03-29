d6cbcc5cda3c87e1f42c1c3fc7185f82c881868f
Andrew Piper <andrew@threax.com>
2019-03-29 18:07:59 -0400

    Disabled the license manager.

--- Standalone/Controller/License/LicenseManager.cs
+++ Standalone/Controller/License/LicenseManager.cs
@@ -18,10 +18,6 @@ namespace Medical
     {
         private String keyFile;
 
-        private AnomalousLicense license;
-        private String keyDialogMessage = null;
-        private String keyDialogUserName = null;
-
         public LicenseManager(String keyFile)
         {
             this.keyFile = keyFile;
@@ -29,137 +25,17 @@ namespace Medical
 
         public void getKey(Action<bool> resultCallback)
         {
-#if ALLOW_OVERRIDE
-            if (MedicalConfig.Cracked)
-            {
-                Logging.Log.ImportantInfo("Running with no copy protection");
-                ThreadManager.invoke(() => resultCallback(true));
-                return;
-            }
-#endif
-            ThreadPool.QueueUserWorkItem(state =>
-            {
-                if (File.Exists(keyFile))
-                {
-                    if (MedicalConfig.StoreCredentials)
-                    {
-                        using (Stream stream = File.OpenRead(keyFile))
-                        {
-                            try
-                            {
-                                //Load old license
-                                byte[] fileContents = new byte[stream.Length];
-                                stream.Read(fileContents, 0, fileContents.Length);
-                                license = new AnomalousLicense(fileContents);
-                            }
-                            catch (Exception)
-                            {
-                                license = null;
-                                keyDialogMessage = "Local license corrupted. Please sign in again.";
-                            }
-                        }
-                        //Validate credentials by getting the license from the server again
-                        if (license != null)
-                        {
-                            try
-                            {
-                                AnomalousLicenseServer licenseServer = new AnomalousLicenseServer(MedicalConfig.LicenseServerURL);
-                                byte[] licenseBytes = licenseServer.createLicenseFile(license.User, license.Pass);
-                                if (licenseBytes != null)
-                                {
-                                    storeLicenseFile(licenseBytes);
-                                    license = new AnomalousLicense(licenseBytes);
-                                }
-                                else
-                                {
-                                    //Null the license. Something was not valid from the server.
-                                    keyDialogUserName = license.User;
-                                    keyDialogMessage = "License has expired. Please log in again.";
-                                    license = null;
-
-                                    //Delete the old license
-                                    deleteLicense();
-                                }
-                            }
-                            catch (AnomalousLicenseServerException alse)
-                            {
-                                //Null the license. Something was not valid from the server.
-                                keyDialogMessage = String.Format("License has expired. Reason {0}. Please log in again.", alse.Message);
-                                license = null;
-
-                                //Delete the old license
-                                deleteLicense();
-                            }
-                            catch (Exception)
-                            {
-                                //Do Nothing, allow license that was loaded to be used.
-                            }
-                        }
-                    }
-                    else
-                    {
-                        deleteLicense();
-                    }
-                }
-                //Check validity
-                ThreadManager.invoke(() => resultCallback(Valid));
-            });
+            ThreadManager.invoke(() => resultCallback(true));
         }
 
         public bool allowFeature(long featureCode)
         {
-#if ALLOW_OVERRIDE
-            if (MedicalConfig.Cracked)
-            {
-                return true;
-            }
-
-            if (featureCode == -1)
-            {
-                return true;
-            }
-#endif
-
-            //Switch for features always supported
-            switch (featureCode)
-            {
-                case 0: //Main plugin
-                case 1: //Premium
-                case 2: //Dental Sim
-                case 28: //Lecture Plugin
-                case 29: //Intro tutorial
-                #if ALLOW_OVERRIDE
-                case 6: //Editor Tools
-                case 8: //Developer Tools
-                case 48: //Movement
-                #endif
-                    return true;
-            }
-
-            if (license != null)
-            {
-                return license.supportsFeature(featureCode);
-            }
-            return false;
+            return true;
         }
 
         public bool allowPropUse(long? propId)
         {
-            if(propId.HasValue)
-            {
-                if(license != null)
-                {
-                    return license.allowPropUse(propId.Value);
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return true;
-            }
+            return true;
         }
 
         /// <summary>
@@ -170,14 +46,7 @@ namespace Medical
         /// <returns></returns>
         public bool getNewLicense()
         {
-            try
-            {
-                return login(license.User, license.Pass);
-            }
-            catch (AnomalousLicenseServerException)
-            {
-                return false;
-            }
+            return true;
         }
 
         /// <summary>
@@ -190,80 +59,19 @@ namespace Medical
         /// <exception cref="AnomalousLicenseServerException">If something goes wrong connecting to the server.</exception>
         public bool login(String user, String pass)
         {
-            try
-            {
-                AnomalousLicenseServer licenseServer = new AnomalousLicenseServer(MedicalConfig.LicenseServerURL);
-                byte[] licenseBytes = licenseServer.createLicenseFile(user, pass);
-                if (licenseBytes != null)
-                {
-                    storeLicenseFile(licenseBytes);
-                    license = new AnomalousLicense(licenseBytes);
-                }
-                else
-                {
-                    return false;
-                }
-                return true;
-            }
-            catch (AnomalousLicenseServerException alse)
-            {
-                throw alse;
-            }
-            catch (WebException webException)
-            {
-                switch (webException.Status)
-                {
-                    case WebExceptionStatus.TrustFailure:
-                        throw new AnomalousLicenseServerException("Error establishing SSL trust relationship with server. Please try again on another internet connection.");
-                    case WebExceptionStatus.ConnectFailure:
-                        throw new AnomalousLicenseServerException("Cannot connect to License Server. Please try again later.");
-                    case WebExceptionStatus.NameResolutionFailure:
-                        throw new AnomalousLicenseServerException("Could not find host name. Please try again later.");
-                    case WebExceptionStatus.Timeout:
-                        throw new AnomalousLicenseServerException("Connection to license server timed out. Please try again later.");
-                    default:
-                        throw new AnomalousLicenseServerException(String.Format("An undefined error occured connecting to the license server. Please try again later. The status was {0}.", webException.Status));
-                }
-            }
-            catch (LicenseInvalidException ex)
-            {
-                throw new AnomalousLicenseServerException(String.Format("License returned from server is invalid.\nReason: {0}\nPlease contact support at CustomerService@AnomalousMedical.com.", ex.Message));
-            }
-            catch (Exception e)
-            {
-                throw new AnomalousLicenseServerException(String.Format("Could not connect to license server. Please try again later.\nReason: {0}", e.Message));
-            }
+            return true;
         }
 
         public void deleteLicense()
         {
-#if ALLOW_OVERRIDE
-            if (MedicalConfig.Cracked)
-            {
-                return;
-            }
-#endif
-            try
-            {
-                File.Delete(keyFile);
-            }
-            catch (Exception)
-            {
 
-            }
         }
 
         public bool Valid
         {
             get
             {
-#if ALLOW_OVERRIDE
-                if (MedicalConfig.Cracked)
-                {
-                    return true;
-                }
-#endif
-                return license != null;
+                return true;
             }
         }
 
@@ -271,13 +79,7 @@ namespace Medical
         {
             get
             {
-#if ALLOW_OVERRIDE
-                if (MedicalConfig.Cracked)
-                {
-                    return "Anomalous Medical Internal";
-                }
-#endif
-                return license != null ? license.LicenseeName : "Invalid";
+                return "Anomalous Medical";
             }
         }
 
@@ -285,13 +87,7 @@ namespace Medical
         {
             get
             {
-#if ALLOW_OVERRIDE
-                if (MedicalConfig.Cracked)
-                {
-                    return "AnomalousMedicalInternal";
-                }
-#endif
-                return license != null ? license.User : "Invalid";
+                return "User";
             }
         }
 
@@ -299,7 +95,7 @@ namespace Medical
         {
             get
             {
-                return license != null ? license.Pass : "";
+                return "";
             }
         }
 
@@ -307,7 +103,7 @@ namespace Medical
         {
             get
             {
-                return keyDialogUserName;
+                return "";
             }
         }
 
@@ -315,18 +111,7 @@ namespace Medical
         {
             get
             {
-                return keyDialogMessage;
-            }
-        }
-
-        private void storeLicenseFile(byte[] license)
-        {
-            if (MedicalConfig.StoreCredentials)
-            {
-                using (Stream fileStream = new FileStream(keyFile, FileMode.Create))
-                {
-                    fileStream.Write(license, 0, license.Length);
-                }
+                return "";
             }
         }
     }
