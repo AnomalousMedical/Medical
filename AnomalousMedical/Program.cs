79bb2697192bb286b6c335f3b0c97b99628b34eb
Andrew Piper <andrew@threax.com>
2019-07-27 17:00:04 -0400

    Don't handle dpi awareness. Enalbe all plugins.

--- AnomalousMedical/Program.cs
+++ AnomalousMedical/Program.cs
@@ -11,28 +11,11 @@ namespace Medical
     public static class Program
     {
         /// <summary>
-        /// Calling this function will set the dpi awareness. It should be done first.
-        /// </summary>
-        /// <returns></returns>
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern bool SetProcessDPIAware();
-
-        /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         public static void Main()
         {
-            SetProcessDPIAware();
-
-            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12; //Enable TLS 1.1 and 1.2
-
-            var osVersion = Environment.OSVersion.Version;
-            if (!(osVersion.Major == 6 && osVersion.Minor == 0)) //Only disable if not on vista (6.0) we don't worry about earlier versions since this won't even run on xp and vista's days are numbered.
-            {
-                ServicePointManager.SecurityProtocol &= ~SecurityProtocolType.Tls11;
-            }
-
             WindowsRuntimePlatformInfo.Initialize();
             OgrePlugin.OgreInterface.CompressedTextureSupport = OgrePlugin.CompressedTextureSupport.None;
 
