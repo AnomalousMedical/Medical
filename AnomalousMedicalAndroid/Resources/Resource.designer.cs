caadcbf92bd3b5ed07cb88a6e2ce6b8b6a15273f
Andrew Piper <andrew@threax.com>
2019-12-21 16:50:27 -0500

    Got Android versions working with ms di instead of Autofac.

--- AnomalousMedicalAndroid/Resources/Resource.designer.cs
+++ AnomalousMedicalAndroid/Resources/Resource.designer.cs
@@ -44,8 +44,8 @@ namespace AnomalousMedicalAndroid
 		public partial class Drawable
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7F010000
+			public const int Icon = 2130771968;
 			
 			static Drawable()
 			{
@@ -60,8 +60,8 @@ namespace AnomalousMedicalAndroid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060000
-			public const int editText1 = 2131099648;
+			// aapt resource value: 0x7F020000
+			public const int editText1 = 2130837504;
 			
 			static Id()
 			{
@@ -76,7 +76,7 @@ namespace AnomalousMedicalAndroid
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
+			// aapt resource value: 0x7F030000
 			public const int Main = 2130903040;
 			
 			static Layout()
@@ -92,7 +92,7 @@ namespace AnomalousMedicalAndroid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040000
+			// aapt resource value: 0x7F040000
 			public const int app_name = 2130968576;
 			
 			static String()
@@ -108,7 +108,7 @@ namespace AnomalousMedicalAndroid
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f050000
+			// aapt resource value: 0x7F050000
 			public const int AnomalousMedicalTheme = 2131034112;
 			
 			static Style()
